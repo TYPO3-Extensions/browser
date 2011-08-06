470f359ea3aa860d7bee81121a255dc3a3af75fa
dwildt <dwildt@735d13b6-9817-0410-8766-e36946ffe9aa>
2011-08-06 12:37:19 +0000

    3.7.0 record-browser 0.0.6
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/browser/trunk@50652 735d13b6-9817-0410-8766-e36946ffe9aa

--- res/js/jquery/plugins/jquery.t3browser/jquery.t3browser-0.0.1.js
+++ res/js/jquery/plugins/jquery.t3browser/jquery.t3browser-0.0.1.js
@@ -24,45 +24,70 @@
     hide :    function( )
               {
               },
-    update :  function( id, url )
+    update :  function( html_element, url )
               {
-                //$(id).slideUp("slow");
-                $("#loading").show();
-                $(id).addClass("loading");
-                $(id).prepend("\t<div id='txbrowserpi1loader'></div>\n");
-                $("#txbrowserpi1loader").fadeOut(0).fadeIn(150);
-                $(id).load(
+                  // Add an opacity to the html element
+                $(html_element).addClass("opacity08");
+
+                  // Cover the html element with a loading gif
+                $(html_element).prepend("\t<div id='tx-browser-pi1-loader'></div>\n");   
+                  // Get the size of the html element
+                var heightWiPx  = $(html_element).css('height');
+                var widthWiPx   = $(html_element).css('width');
+                  // Set the loader to the size of the html element
+                $("#tx-browser-pi1-loader").css('height', heightWiPx);
+                $("#tx-browser-pi1-loader").css('width',  widthWiPx));
+                $("#tx-browser-pi1-loader").css('margin-bottom', "-" + heightWiPx);
+                  // Fade in the loader
+                $("#tx-browser-pi1-loader").fadeIn(150);
+                  // Cover the html element with a loading gif
+
+                  // Send the AJAX request
+                  // Replace the content of the html element with the delivered data
+                $(html_element).load(
                   url,
                   function(responseText, textStatus, XMLHttpRequest)
                   {
-                    $("#loading").hide();
-                    $(id).removeClass("loading");
-                    $("#txbrowserpi1loader").fadeOut(500, function()
+                      // Fade out the loader
+                    $("#tx-browser-pi1-loader").fadeOut(500, function()
                     {
                       $(this).remove();
                     });
+                      // Remove the opacity of the html element
+                    $(html_element).removeClass("opacity08");
                   }
                 );
+                  // Send the AJAX request
               }
+                // update() 
   };
   
   $.fn.t3browser = function( method )
   {
+      // See http://docs.jquery.com/Plugins/Authoring#Plugin_Methods
+
       // Method calling logic
-    if ( methods[method] ) {
+    if ( methods[method] ) 
+    {
       return methods[ method ].apply( this, Array.prototype.slice.call( arguments, 1 ));
-    } else if ( typeof method === 'object' || ! method ) {
+    }
+    else if ( typeof method === 'object' || ! method )
+    {
       return methods.init.apply( this, arguments );
-    } else {
+    }
+    else
+    {
       $.error( 'Method ' +  method + ' does not exist on jQuery.tooltip' );
     }    
   };
+      // Method calling logic
 
 })( jQuery );
 
+
 $( document ).ready( function( )
 {
-    // User has clicked on an item of the record browser
+    // User has clicked the record browser
     // live: Attach a handler to the event for all elements which match the current selector, now and in the future.
     //       see: http://api.jquery.com/live/
   $(".c2479-record-browser").live(    
@@ -74,4 +99,6 @@ $( document ).ready( function( )
       $(this).t3browser('update', "#c2479-singleview-1", $(this).attr("href") + "?type=28562 #c2479-singleview-1 > *");
     }
   );
+    // User has clicked the record browser
+
 });
