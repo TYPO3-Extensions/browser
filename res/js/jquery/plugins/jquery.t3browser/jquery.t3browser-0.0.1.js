94ae4a81dc02e95113d3f720626e3784c5362881
dwildt <dwildt@735d13b6-9817-0410-8766-e36946ffe9aa>
2011-08-06 16:10:13 +0000

    3.7.0 record-browser 0.0.6
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/browser/trunk@50654 735d13b6-9817-0410-8766-e36946ffe9aa

--- res/js/jquery/plugins/jquery.t3browser/jquery.t3browser-0.0.1.js
+++ res/js/jquery/plugins/jquery.t3browser/jquery.t3browser-0.0.1.js
@@ -1,5 +1,5 @@
 /**
- * jQuery t3_browser Plugin 0.0.1
+ * jQuery t3browser Plugin 0.0.1
  *
  * http://docs.jquery.com/Plugins/t3browser
  *
@@ -83,22 +83,3 @@
       // Method calling logic
 
 })( jQuery );
-
-
-$( document ).ready( function( )
-{
-    // User has clicked the record browser
-    // live: Attach a handler to the event for all elements which match the current selector, now and in the future.
-    //       see: http://api.jquery.com/live/
-  $( ".c2479-record-browser" ).live(    
-    'click',
-    function( e ) {
-        // Don't execute the click
-      e.preventDefault( );
-        // Update the content with the id #c2479-singleview-1
-      $( this ).t3browser( 'update', "#c2479-singleview-1", $( this ).attr( "href" ) + "?type=28562 #c2479-singleview-1 > *" );
-    }
-  );
-    // User has clicked the record browser
-
-});
