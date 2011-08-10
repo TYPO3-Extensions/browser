e3a1e6c04540d53f02f4b84e534ef05223710b5e
dwildt <dwildt@735d13b6-9817-0410-8766-e36946ffe9aa>
2011-08-10 15:15:24 +0000

    3.7.0 jquery.t3browser localization 0.0.3
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/browser/trunk@50940 735d13b6-9817-0410-8766-e36946ffe9aa

--- res/js/jquery/plugins/jquery.t3browser/localization.de.js
+++ res/js/jquery/plugins/jquery.t3browser/localization.de.js
@@ -29,7 +29,7 @@ $( document ).ready( function( )
       errMissingTagPropertyPrmpt: "Ein HTML Tag mit dem Attribute {0} fehlt. AJAX wird nicht korrekt funktionieren!",
       errServerErrorPrmpt:        "Das ist eine Nachricht vom Server. Vielleicht hat der Server ein grundsätzliches Problem. Falls er mehr als die Fehlermeldung sendet, siehst Du die Server-Antwort unter diesen Meldungen.",
       hlpForumLabel:              "Interaktive Hilfe:",
-      hlpForumPrmpt:              "Wenn Du den Fehler nicht beheben kannst, kannst Du Hilfe im TYPO3-Browser-Forum auf <a href=\"http://typo3-browser-forum.de/\">typo3-browser.forum.de</a> bekommen.",
+      hlpForumPrmpt:              "Wenn Du den Fehler nicht beheben kannst, kannst Du Hilfe im TYPO3-Browser-Forum auf <a href=\"http://typo3-browser-forum.de/\">typo3-browser-forum.de</a> bekommen.",
       hlpMissingTagPropertyLabel: "Prüfe das HTML-Template:",
       hlpMissingTagPropertyPrmpt: "Bitte ergänze das Template mit etwas wie <div id=\"{0}\">...</div>",
       hlpPageObjectLabel:         "Du kannst TYPO3 prüfen:",
