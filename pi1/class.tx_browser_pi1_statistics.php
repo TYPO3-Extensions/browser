233b96cc4126e3e383899015b3c935422a087192
dwildt <dwildt@735d13b6-9817-0410-8766-e36946ffe9aa>
2011-11-07 21:34:38 +0000

    3.9.3 initialisation of statistic module
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/browser/trunk@53850 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ pi1/class.tx_browser_pi1_statistics.php
@@ -0,0 +1,354 @@
+<?php
+
+/***************************************************************
+*  Copyright notice
+*
+*  (c) 2011 - Dirk Wildt <http://wildt.at.die-netzmacher.de>
+*  All rights reserved
+*
+*  This script is part of the TYPO3 project. The TYPO3 project is
+*  free software; you can redistribute it and/or modify
+*  it under the terms of the GNU General Public License as published by
+*  the Free Software Foundation; either version 2 of the License, or
+*  (at your option) any later version.
+*
+*  The GNU General Public License can be found at
+*  http://www.gnu.org/copyleft/gpl.html.
+*
+*  This script is distributed in the hope that it will be useful,
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
+*  GNU General Public License for more details.
+*
+*  This copyright notice MUST APPEAR in all copies of the script!
+***************************************************************/
+
+/**
+* The class tx_browser_pi1_statistics bundles methods for statistics requirement
+*
+* @author    Dirk Wildt <http://wildt.at.die-netzmacher.de>
+* @package    TYPO3
+* @subpackage    tx_browser
+*
+* @version 3.9.3
+* @since 3.9.3
+*/
+
+/**
+ * [CLASS/FUNCTION INDEX of SCRIPT]
+ *
+ *
+ */
+class tx_browser_pi1_statistics
+{
+    //////////////////////////////////////////////////////
+    //
+    // Variables set by the pObj (by class.tx_browser_pi1.php)
+
+    // [Array] The current TypoScript configuration array
+  var $conf       = false;
+    // [Integer] The current mode (from modeselector)
+  var $mode       = false;
+    // [String] 'list' or 'single': The current view
+  var $view       = false;
+    // [Array] The TypoScript configuration array of the current view
+  var $conf_view  = false;
+    // [String] TypoScript path to the current view. I.e. views.single.1
+  var $conf_path  = false;
+    // Variables set by the pObj (by class.tx_browser_pi1.php)
+
+
+
+    //////////////////////////////////////////////////////
+    //
+    // Variables set by this class
+
+    // [Boolean] True, if session management is enabled. Will set while runtime
+  //var $bool_session_enabled = null;
+    // Variables set by this class
+
+
+
+
+
+
+
+
+
+  /**
+ * Constructor. The method initiate the parent object
+ *
+ * @param object    The parent object
+ * @return  void
+ */
+  function __construct($pObj)
+  {
+    $this->pObj = $pObj;
+  }
+
+
+
+
+
+
+
+
+
+  /***********************************************
+  *
+  * Initial
+  *
+  **********************************************/
+
+
+
+
+
+
+
+
+
+  /**
+ * countViewSingleRecord( ):  Sets the global $bool_session_enabled.
+ *                            The boolean is controlled by the flexform / TypoScript.
+ *                            The User can enable and disable session management.
+ *
+ * @return  void
+ * @version 3.9.3
+ * @since 3.9.3
+ */
+  public function countViewSingleRecord( )
+  {
+      ///////////////////////////////////////////////////////////////////////////////
+      //
+      // RETURN: Boolean is set before
+
+    $pos = strpos($this->pObj->str_developer_csvIp, t3lib_div :: getIndpEnv('REMOTE_ADDR'));
+    if ( ! ( $pos === false ) )
+    {
+      var_dump(__METHOD__. ' (' . __LINE__ . '): ', $this->pObj->piVars);
+      die( );
+    }
+      // RETURN: Boolean is set before
+
+
+
+    return;
+  }
+
+
+
+
+
+
+
+
+
+  /**
+ * getNameOfDataSpace( ): Get the name of the session data space.
+ *                        The name is user, if a frontend user is logged in.
+ *                        The name is ses, if any frontend user isn't logged in.
+ *
+ * @return  string    user || ses
+ * @version 3.9.3
+ * @since 3.9.3
+ */
+  public function getNameOfDataSpace( )
+  {
+    switch( $GLOBALS['TSFE']->loginUser )
+    {
+      case( true ):
+        return 'user';
+      default:
+        return 'ses';
+    }
+  }
+
+
+
+
+
+
+
+
+  /***********************************************
+  *
+  * Cache
+  *
+  **********************************************/
+
+
+
+
+
+
+
+
+
+  /**
+ * cacheOfListView(): The method caches list view data in the session cache.
+ *                    The result will stored in the global $this->pObj->uids_of_all_rows too.
+ *                    If session managment is disabled, the method will call the list view method.
+ *                    The result will stored in the global $this->pObj->uids_of_all_rows too but
+ *                    without any caching.
+ *
+ * @return  void
+ * @version 3.9.3
+ * @since 3.9.3
+ */
+  public function cacheOfListView( )
+  {
+      //////////////////////////////////////////////////////////////////////////
+      //
+      // Set status of the session management
+
+    $this->sessionIsEnabled( );
+      // Set status of the session management
+
+
+      // Uid of the current plugin
+    $tt_content_uid = $this->pObj->cObj->data['uid'];
+
+
+
+      //////////////////////////////////////////////////////////////////////////
+      //
+      // RETURN: no session, get data from the list view. Call it!
+
+    if( ! $this->bool_session_enabled )
+    {
+      if ($this->pObj->b_drs_perform || $this->pObj->b_drs_session)
+      {
+        t3lib_div::devlog('[WARN/PERFORMANCE+SESSION] list view is called. Uids of all rows are needed. '.
+          'Be aware of less performance!', $this->pObj->extKey, 2);
+        t3lib_div::devlog('[HELP/PERFORMANCE+SESSION] Enable session for better performance!', $this->pObj->extKey, 1);
+      }
+        // listView will set $this->pObj->uids_of_all_rows[$tt_content_uid]['cache']['mode-' . $this->mode]['uids_of_all_rows']
+      $this->pObj->objNav->recordbrowser_callListView();
+// dwildt, 111107
+      return;
+    }
+      // RETURN: no session, get data from the list view. Call it!
+
+
+
+      //////////////////////////////////////////////////////////////////////////
+      //
+      // DRS - Development Reporting System
+
+    if ($this->pObj->b_drs_session || $this->pObj->b_drs_templating)
+    {
+      t3lib_div::devlog('[INFO/SESSION+TEMPLATING] Session: uid of all rows should delivered by the session data '.
+        '(best performance).', $this->pObj->extKey, 0);
+    }
+      // DRS - Development Reporting System
+
+
+
+      //////////////////////////////////////////////////////////////////////////
+      //
+      // Get the name of the session data space
+
+    $str_data_space = $this->getNameOfDataSpace( );
+      // Get the name of the session data space
+
+
+
+      //////////////////////////////////////////////////////////////////////////
+      //
+      // Get tx_browser-pi1 session data
+
+    $arr_browser_session = $GLOBALS['TSFE']->fe_user->getKey($str_data_space, $this->pObj->prefixId);
+      // Get tx_browser-pi1 session data
+
+
+
+      //////////////////////////////////////////////////////////////////////////
+      //
+      // DRS - Development Reporting System
+
+    if( empty( $arr_browser_session[$tt_content_uid]['cache']['mode-' . $this->mode]['uids_of_all_rows'] ) )
+    {
+      if ($this->pObj->b_drs_warn)
+      {
+        t3lib_div::devlog('[INFO/WARN] Session array [' . $str_data_space . ']' .
+          '[' . $this->pObj->prefixId . '][' . $tt_content_uid . '][mode-' . $this->mode . '][uids_of_all_rows] is empty!',
+          $this->pObj->extKey, 2);
+      }
+    }
+    if( ! empty( $arr_browser_session[$tt_content_uid]['cache']['mode-' . $this->mode]['uids_of_all_rows'] ) )
+    {
+      if ($this->pObj->b_drs_session || $this->pObj->b_drs_templating)
+      {
+        t3lib_div::devlog('[INFO/SESSION+TEMPLATING] Session array [' . $str_data_space . ']' .
+          '[' . $this->pObj->prefixId . '][' . $tt_content_uid . '][mode-' . $this->mode . '][uids_of_all_rows] is set with ' .
+          '#' . count($arr_browser_session[$tt_content_uid]['cache']['mode-' . $this->mode]['uids_of_all_rows']) . ' uids.',  $this->pObj->extKey, 0);
+      }
+    }
+      // DRS - Development Reporting System
+
+
+
+      //////////////////////////////////////////////////////////////////////////
+      //
+      // Session data is not set: set tx_browser_pi1['uids_of_all_rows'] !
+
+    if( ! isset( $arr_browser_session[$tt_content_uid]['cache']['mode-' . $this->mode]['uids_of_all_rows'] ) )
+    {
+        // listView will set $this->pObj->uids_of_all_rows[$tt_content_uid]['cache']['mode-' . $this->mode]['uids_of_all_rows']
+      $this->pObj->objNavi->recordbrowser_callListView( );
+        // Set the session array uids_of_all_rows
+      $arr_browser_session[$tt_content_uid]['cache']['mode-' . $this->mode]['uids_of_all_rows'] = $this->pObj->uids_of_all_rows[$tt_content_uid]['cache']['mode-' . $this->mode]['uids_of_all_rows'];
+        // Write session data tx_browser_pi1
+      $GLOBALS['TSFE']->fe_user->setKey($str_data_space, $this->pObj->prefixId, $arr_browser_session);
+        // DRS - Development Reporting System
+      if ( $this->pObj->b_drs_session || $this->pObj->b_drs_templating )
+      {
+        t3lib_div::devlog('[INFO/SESSION+TEMPLATING] Session array [' . $str_data_space . ']' .
+          '[' . $this->pObj->prefixId . '][' . $tt_content_uid . '][mode-' . $this->mode . '][uids_of_all_rows] is set with ' .
+          '#' . count($this->pObj->uids_of_all_rows[$tt_content_uid]['cache']['mode-' . $this->mode]['uids_of_all_rows']) . ' uids.',  $this->pObj->extKey, 0);
+      }
+// dwildt, 111107
+//          // Get uids of all records
+//        $this->pObj->uids_of_all_rows[$tt_content_uid]['cache']['mode-' . $this->mode]['uids_of_all_rows'] = $arr_browser_session[$tt_content_uid]['cache']['mode-' . $this->mode]['uids_of_all_rows'];
+    }
+      // Session data is not set: set tx_browser_pi1['uids_of_all_rows'] !
+
+
+
+      //////////////////////////////////////////////////////////////////////////
+      //
+      // Set the global $this->pObj->uids_of_all_rows
+
+    $this->pObj->uids_of_all_rows[$tt_content_uid]['cache']['mode-' . $this->mode]['uids_of_all_rows'] = $arr_browser_session[$tt_content_uid]['cache']['mode-' . $this->mode]['uids_of_all_rows'];
+    return;
+      // Set the global $this->pObj->uids_of_all_rows
+  }
+
+
+
+
+
+
+
+
+  /***********************************************
+  *
+  * Statistics
+  *
+  **********************************************/
+
+
+
+
+
+
+
+
+
+}
+
+if (defined('TYPO3_MODE') && $TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/browser/pi1/class.tx_browser_pi1_statistics.php']) {
+  include_once ($TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/browser/pi1/class.tx_browser_pi1_statistics.php']);
+}
+?>
