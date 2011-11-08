0094937f6c8c9a24539ec36955e945a7e93fae5b
dwildt <dwildt@735d13b6-9817-0410-8766-e36946ffe9aa>
2011-11-08 16:15:29 +0000

    3.9.3 initialisation of statistic module
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/browser/trunk@53908 735d13b6-9817-0410-8766-e36946ffe9aa

--- pi1/class.tx_browser_pi1_statistics.php
+++ pi1/class.tx_browser_pi1_statistics.php
@@ -538,7 +538,7 @@ class tx_browser_pi1_statistics
       if( $this->debugging )
       {
         $str_warn    = '<p style="border: 1em solid red; background:white; color:red; font-weight:bold; text-align:center; padding:2em;">'.$this->pObj->pi_getLL('drs_security').'</p>';
-        $str_header  = '<h1 style="color:red">'.$this->pObj->pi_getLL('error_sql_h1').'</h1>';
+        $str_header  = '<h1 style="color:red">'.$this->pObj->pi_getLL('warn_sql_h1').'</h1>';
         $str_prompt  = '<p style="font-family:monospace;font-size:smaller;padding-top:2em;">Any row isn\'t affected!</p>';
         $str_prompt .= '<p style="font-family:monospace;font-size:smaller;padding-top:2em;">'.$query.'</p>';
         echo $str_warn.$str_header.$str_prompt;
