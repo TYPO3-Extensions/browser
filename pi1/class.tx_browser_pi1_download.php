9ce447dbd3595989dc812a1c3c4fb5a02da52186
dwildt <dwildt@735d13b6-9817-0410-8766-e36946ffe9aa>
2011-11-14 17:01:18 +0000

    3.9.3 initialisation of download module
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/browser/trunk@54127 735d13b6-9817-0410-8766-e36946ffe9aa

--- pi1/class.tx_browser_pi1_download.php
+++ pi1/class.tx_browser_pi1_download.php
@@ -718,7 +718,7 @@ class tx_browser_pi1_download
 
       // Count the hit
     $field = $this->pObj->objStat->fieldDownloadsByVisits;
-    $this->pObj->objStat->sql_update_statistics( $this->table, $field, $this->uid, $operator );
+    $this->pObj->objStat->sql_update_statistics( $this->table, $this->field, $this->uid, $operator );
 
     $pos = strpos($this->pObj->str_developer_csvIp, t3lib_div :: getIndpEnv('REMOTE_ADDR'));
     if ( ! ( $pos === false ) )
