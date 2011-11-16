7f6506aff6d10aecaddfc05aff98da40bbd44474
dwildt <dwildt@735d13b6-9817-0410-8766-e36946ffe9aa>
2011-11-16 17:40:32 +0000

    3.9.3 extend download module: display record for every language
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/browser/trunk@54267 735d13b6-9817-0410-8766-e36946ffe9aa

--- lib/class.tx_browser_cssstyledcontent.php
+++ lib/class.tx_browser_cssstyledcontent.php
@@ -156,7 +156,7 @@ class tx_browser_cssstyledcontent extends tx_cssstyledcontent_pi1
     $pos = strpos($this->str_developer_csvIp, t3lib_div :: getIndpEnv('REMOTE_ADDR'));
     if ( ! ( $pos === false ) )
     {
-      var_dump(__METHOD__. ' (' . __LINE__ . '): ' , $bool_drs, $this->extKey );
+      var_dump(__METHOD__. ' (' . __LINE__ . '): ' , $conf['userFunc.']['drs'], $conf['userFunc.']['drs.'], $bool_drs, $this->extKey );
       //exit;
     }
 
