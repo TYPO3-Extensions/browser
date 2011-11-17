3efe583965cff8febca6205026983ebdb49b09bd
dwildt <dwildt@735d13b6-9817-0410-8766-e36946ffe9aa>
2011-11-17 17:45:29 +0000

    3.9.3 download module: localisation
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/browser/trunk@54408 735d13b6-9817-0410-8766-e36946ffe9aa

--- lib/class.tx_browser_cssstyledcontent.php
+++ lib/class.tx_browser_cssstyledcontent.php
@@ -504,6 +504,7 @@ class tx_browser_cssstyledcontent extends tx_cssstyledcontent_pi1
 //                            );
 // dwildt, 111106, -
 // dwildt, 111106, +
+        $marker                             = null;
         $marker['###SYS_LANGUAGE.FLAG###']  = $GLOBALS['TSFE']->lang;
         if( empty( $marker['###SYS_LANGUAGE.FLAG###'] ) )
         {
