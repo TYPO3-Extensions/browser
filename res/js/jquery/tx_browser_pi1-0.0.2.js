be714493d67d5c8cc354025b36edb8328226281b
dwildt <dwildt@735d13b6-9817-0410-8766-e36946ffe9aa>
2011-11-21 22:36:20 +0000

    3.9.3 download module layout
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/browser/trunk@54578 735d13b6-9817-0410-8766-e36946ffe9aa

--- res/js/jquery/tx_browser_pi1-0.0.2.js
+++ res/js/jquery/tx_browser_pi1-0.0.2.js
@@ -22,7 +22,7 @@ $( document ).ready( function( )
     //
     // UI for buttons in the searchbox form
 
-  $( "button, input:submit, input:button, a.backbutton, a.iconbutton", ".tx-browser-pi1" ).button( );
+  $( "button, input:submit, input:button, a.backbutton, div.iconbutton", ".tx-browser-pi1" ).button( );
     // UI for buttons in the searchbox form
 
 
