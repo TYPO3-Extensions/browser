eec99c6f2ac6ba78f46c884755a6694545ba33a2
dwildt <dwildt@735d13b6-9817-0410-8766-e36946ffe9aa>
2011-11-28 21:06:09 +0000

    3.9.3: AJAX old object: integration of method button
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/browser/trunk@55062 735d13b6-9817-0410-8766-e36946ffe9aa

--- res/js/tx_browser_pi1_ajax-0.0.2.js
+++ res/js/tx_browser_pi1_ajax-0.0.2.js
@@ -349,7 +349,7 @@ function ajaxifySingleLinks(pObj) {
           }
           targetObj.slideUp(300, function() {
             $(this).replaceWith(d).queue( function () {
-              $( "button, input:submit, input:button, a.backbutton, div.iconbutton", ".tx-browser-pi1" ).button( );
+              //$( "button, input:submit, input:button, a.backbutton, div.iconbutton", ".tx-browser-pi1" ).button( );
               alert('ajaxifySingleLinks');
             });
             pObj.find('.singleview').slideUp(1, function() {
