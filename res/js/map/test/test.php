bb91d8019db58ab0415530d6c97414d96df41e12
dwildt <dwildt@735d13b6-9817-0410-8766-e36946ffe9aa>
2011-12-19 23:06:15 +0000

    3.9.6: open street map
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/browser/trunk@55741 735d13b6-9817-0410-8766-e36946ffe9aa

--- res/js/map/test/test.php
+++ res/js/map/test/test.php
@@ -2,8 +2,8 @@
 header('Content-type: plain/txt'); 
 echo "
 lat	lon	title	description	icon	iconSize	iconOffset
-48.9459301	9.6075669	Title One	Description one<br>Second line.<br><br>(click again to close)	./img/test2.png	14,14	0,-24
-48.9899851	9.5382032	Title Two	Description two	./img/test2.png	14,14	-8,-8
+48.9459301	9.6075669	Title One	Description one<br>Second line.<br><br>(click again to close)	typo3conf/ext/browser/res/js/map/test/img/test2.png	14,14	0,-24
+48.9899851	9.5382032	Title Two	Description two	typo3conf/ext/browser/res/js/map/test/img/test2.png	14,14	-8,-8
 
 ";
 ?>
