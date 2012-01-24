996add0ce2d333484c8e052ba29a349e222934d5
dwildt <dwildt@735d13b6-9817-0410-8766-e36946ffe9aa>
2012-01-24 23:56:05 +0000

    3.9.6 - AJAX development
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/browser/trunk@56665 735d13b6-9817-0410-8766-e36946ffe9aa

--- res/js/jquery/tx_browser_pi1-0.0.3.js
+++ res/js/jquery/tx_browser_pi1-0.0.3.js
@@ -77,7 +77,10 @@ $( document ).ready( function( )
       }
         // Append the TYPO3 typeNum of the csv export page object
       $( "#c###TT_CONTENT.UID###-list-searchbox-form-###MODE### fieldset" )
-        .append( '<input id="c###TT_CONTENT.UID###-button-export-###MODE###" type="hidden" name="type" value="###TYPENUM_CSV###" />' );
+        .append(
+          '<input id="c###TT_CONTENT.UID###-button-export-###MODE###" type="hidden" name="type" value="###TYPENUM_CSV###" />'
+        );
+      bool_sendAjaxRequest = false;
     }
   );
     // Append a hidden field with TYPO3 page type for csv export
@@ -86,6 +89,7 @@ $( document ).ready( function( )
     function ( e ) 
     {
       $( "#c###TT_CONTENT.UID###-button-export-###MODE###" ).remove( )
+      bool_sendAjaxRequest = true;
     }
   );
     // Remove the hidden field after submitting the form
