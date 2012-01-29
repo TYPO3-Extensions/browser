278381a6747eb2ea98e1ae6c2e1cbfacdbfa1448
dwildt <dwildt@735d13b6-9817-0410-8766-e36946ffe9aa>
2012-01-29 14:59:46 +0000

    3.9.6 - Improvement DRS
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/browser/trunk@56896 735d13b6-9817-0410-8766-e36946ffe9aa

--- pi1/class.tx_browser_pi1_template.php
+++ pi1/class.tx_browser_pi1_template.php
@@ -767,7 +767,7 @@ class tx_browser_pi1_template
         $template       = $this->pObj->cObj->substituteMarkerArray($template, $markerArray);
         if ($this->pObj->b_drs_warn)
         {
-          t3lib_div::devlog('[WARN/TEMPLATING] There isn\t any row.', $this->pObj->extKey, 0);
+          t3lib_div::devlog('[WARN/TEMPLATING] There isn\t any row.', $this->pObj->extKey, 2);
         }
         return $template;
       }
