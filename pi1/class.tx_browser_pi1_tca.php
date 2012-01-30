a4fee933f21979d8cde03e393534f91ed9ba662a
dwildt <dwildt@735d13b6-9817-0410-8766-e36946ffe9aa>
2012-01-30 02:43:54 +0000

    3.9.6 - Bugfixing handleAs
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/browser/trunk@56939 735d13b6-9817-0410-8766-e36946ffe9aa

--- pi1/class.tx_browser_pi1_tca.php
+++ pi1/class.tx_browser_pi1_tca.php
@@ -716,6 +716,12 @@ class tx_browser_pi1_tca
     $this->maxColumns           = $maxColumns;
     $this->boolSubstitute       = $boolSubstitute;
     $this->arrHandleAs          = $this->pObj->arrHandleAs;
+$pos = strpos( '91.23.174.97' , t3lib_div :: getIndpEnv('REMOTE_ADDR'));
+if (!($pos === false))
+{
+  var_dump( __METHOD__ . ' (line: ' . __LINE__ . ')',  $this->arrHandleAs );
+
+}
       // Set globals
 
       // Set default return array
@@ -1130,12 +1136,12 @@ class tx_browser_pi1_tca
  */
   private function handleAsTimestamp(  )
   {
-$pos = strpos( '91.23.174.97' , t3lib_div :: getIndpEnv('REMOTE_ADDR'));
-if (!($pos === false))
-{
-  var_dump( __METHOD__ . ' (line: ' . __LINE__ . ')',  $this->value );
-
-}
+//$pos = strpos( '91.23.174.97' , t3lib_div :: getIndpEnv('REMOTE_ADDR'));
+//if (!($pos === false))
+//{
+//  var_dump( __METHOD__ . ' (line: ' . __LINE__ . ')',  $this->value );
+//
+//}
       // RETURN tableField isn't content of handleAs['timestamp']
     $pos = strpos( $this->arrHandleAs['timestamp'] , $this->tableField );
     if( $pos === false )
@@ -1198,12 +1204,12 @@ if (!($pos === false))
         // strftime shouldn't moved to ISO
     }
       // $this->value is UTF8
-$pos = strpos( '91.23.174.97' , t3lib_div :: getIndpEnv('REMOTE_ADDR'));
-if (!($pos === false))
-{
-  var_dump( __METHOD__ . ' (line: ' . __LINE__ . ')',  $this->value );
-
-}
+//$pos = strpos( '91.23.174.97' , t3lib_div :: getIndpEnv('REMOTE_ADDR'));
+//if (!($pos === false))
+//{
+//  var_dump( __METHOD__ . ' (line: ' . __LINE__ . ')',  $this->value );
+//
+//}
 
     return;
   }
