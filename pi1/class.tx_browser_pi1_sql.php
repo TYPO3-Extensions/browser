a360311a9f982e648099e8b9512ea1aa04ce5135
dwildt <dwildt@735d13b6-9817-0410-8766-e36946ffe9aa>
2012-02-19 14:19:29 +0000

    3.9.9 - SQL engine 4.x
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/browser/trunk@58135 735d13b6-9817-0410-8766-e36946ffe9aa

--- pi1/class.tx_browser_pi1_sql.php
+++ pi1/class.tx_browser_pi1_sql.php
@@ -115,11 +115,8 @@ class tx_browser_pi1_sql
       // Prompt the expired time to devlog
     $this->pObj->timeTracking_log( __METHOD__, __LINE__,  'begin' );
 
-$my_prompt = array('Hello world', array( 'green', 'red' ) );
-$this->pObj->dev_var_dump( __METHOD__, __LINE__, $my_prompt );
-
-//    $arr_andWhereFilter = $this->pObj->objFilter->andWhere_filter( );
-//    $this->pObj->dev_var_dump( __METHOD__, __LINE__, $arr_andWhereFilter );
+    $arr_andWhereFilter = $this->pObj->objFilter->andWhere_filter( );
+    $this->pObj->dev_var_dump( __METHOD__, __LINE__, $arr_andWhereFilter );
 //
 //      // Prompt the expired time to devlog
 //    $this->pObj->timeTracking_log( __METHOD__, __LINE__,  'after $this->pObj->objFilter->andWhere_filter( )' );
