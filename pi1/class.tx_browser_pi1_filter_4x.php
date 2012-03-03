094b82466146840bafaa34427a39ca50e7a51aa2
dwildt <dwildt@735d13b6-9817-0410-8766-e36946ffe9aa>
2012-03-03 15:34:29 +0000

    3.9.9 - Browser engine 4.x
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/browser/trunk@58892 735d13b6-9817-0410-8766-e36946ffe9aa

--- pi1/class.tx_browser_pi1_filter_4x.php
+++ pi1/class.tx_browser_pi1_filter_4x.php
@@ -1148,6 +1148,13 @@ class tx_browser_pi1_filter_4x {
   {
     static $firstLoop = true;
 
+$cObj_bak = $this->pObj->cObj->data;
+    if( $firstLoop && $this->pObj->b_drs_devTodo )
+    {
+$this->pObj->cObj->data = $this->rows[$uid];
+var_dump( __METHOD__, __LINE__, $this->pObj->cObj->data );
+    }
+
       // Item class
     if($conf_name == 'CATEGORY_MENU')
     {
