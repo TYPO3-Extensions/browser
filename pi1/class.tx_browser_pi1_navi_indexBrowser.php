52ea38f0cfec31080cd859c65ed3cf5b1d5a275f
dwildt <dwildt@735d13b6-9817-0410-8766-e36946ffe9aa>
2012-05-07 00:42:49 +0000

    3.9.13 +++ localisation index browser - step 1
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/browser/trunk@61603 735d13b6-9817-0410-8766-e36946ffe9aa

--- pi1/class.tx_browser_pi1_navi_indexBrowser.php
+++ pi1/class.tx_browser_pi1_navi_indexBrowser.php
@@ -1848,12 +1848,12 @@ class tx_browser_pi1_navi_indexBrowser
       // Get Ids of all (!) translated language records
 
       // Substract uids of default language records, which are translated
-    $arr_rowsDefWoTranslated = array_diff( $arr_rows, $arr_rowsLL[$parentUid] );
+    $arr_rowsDefWoTranslated = array_diff( $arr_rows, ( array ) $arr_rowsLL[$parentUid] );
 
 
     var_dump( __METHOD__, __LINE__, 'array_diff' );
     var_dump( __METHOD__, __LINE__, $arr_rows );
-    var_dump( __METHOD__, __LINE__, $arr_rowsLL[$parentUid] );
+    var_dump( __METHOD__, __LINE__, ( array ) $arr_rowsLL[$parentUid] );
     var_dump( __METHOD__, __LINE__, $arr_rowsDefWoTranslated );
 
       // Add uids of translated recors
