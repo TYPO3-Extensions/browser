6ce1b58a9de076db88529d528bbba944bb637bd1
dwildt <dwildt@735d13b6-9817-0410-8766-e36946ffe9aa>
2012-05-10 01:27:21 +0000

    3.9.13 +++ query list view
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/browser/trunk@61746 735d13b6-9817-0410-8766-e36946ffe9aa

--- pi1/class.tx_browser_pi1_sql_functions.php
+++ pi1/class.tx_browser_pi1_sql_functions.php
@@ -715,7 +715,7 @@ class tx_browser_pi1_sql_functions
       // Enable DRS performance
 
       // Log the time
-    $this->pObj->timeTracking_log( __METHOD__, __LINE__,  'SQL query list view - START' );
+    $this->pObj->timeTracking_log( __METHOD__, __LINE__,  'SQL execute START' );
     $tt_start = $this->pObj->tt_prevEndTime;
 
       // Execute query
@@ -741,7 +741,7 @@ class tx_browser_pi1_sql_functions
       // DRS - Development Reporting System
       
       // Log the time
-    $this->pObj->timeTracking_log( __METHOD__, __LINE__,  'SQL query list view - STOP' );
+    $this->pObj->timeTracking_log( __METHOD__, __LINE__,  'SQL execute STOP' );
     $this->pObj->timeTracking_prompt( $query );
 
       // RESET DRS performance
