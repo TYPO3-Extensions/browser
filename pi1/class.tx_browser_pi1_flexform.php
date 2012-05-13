fffc0d04b2c9e8dc83756059b94cef5d98786e30
dwildt <dwildt@735d13b6-9817-0410-8766-e36946ffe9aa>
2012-05-13 17:41:00 +0000

    3.9.14 +++ general improvements
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/browser/trunk@62124 735d13b6-9817-0410-8766-e36946ffe9aa

--- pi1/class.tx_browser_pi1_flexform.php
+++ pi1/class.tx_browser_pi1_flexform.php
@@ -2877,6 +2877,22 @@ class tx_browser_pi1_flexform {
     }
       // #34212: 120223, dwildt-
     //$conf_limit = $str_limit;
+    list( $start, $results_at_a_time ) = explode( ',', $conf_limit );
+$this->pObj->dev_var_dump( $results_at_a_time );
+    if( $results_at_a_time == null )
+    {
+      if( $this->pObj->b_drs_warn ) 
+      {
+        $prompt = 'views.list.' . $this->mode . 'limit is ' . $conf_limit;
+        t3lib_div :: devlog( '[WARN/DRS] ' . $prompt , $this->pObj->extKey, 2 );
+      }
+      $conf_limit = '0,' . $start; 
+      if( $this->pObj->b_drs_warn ) 
+      {
+        $prompt = 'Please move it from ' . $start . ' to ' . $conf_limit;
+        t3lib_div :: devlog( '[HELP/DRS] ' . $prompt , $this->pObj->extKey, 1 );
+      }
+    }
     
       // Set start
     $pageBrowserPointerLabel  = $this->pObj->conf['navigation.']['pageBrowser.']['pointer'];
@@ -2884,22 +2900,6 @@ class tx_browser_pi1_flexform {
     {
       $multiplier = ( int ) $this->pObj->piVars[$pageBrowserPointerLabel];
       list( $start, $results_at_a_time ) = explode( ',', $conf_limit );
-$this->pObj->dev_var_dump( $results_at_a_time );
-      if( $results_at_a_time == null )
-      {
-        if( $this->pObj->b_drs_warn ) 
-        {
-          $prompt = 'views.list.' . $this->mode . 'limit is ' . $start;
-          t3lib_div :: devlog( '[WARN/DRS] ' . $prompt , $this->pObj->extKey, 2 );
-        }
-        $results_at_a_time  = $start;
-        $start              = 0;
-        if( $this->pObj->b_drs_warn ) 
-        {
-          $prompt = 'Please move it from ' . $results_at_a_time . ' to ' . $start . ',' . $results_at_a_time;
-          t3lib_div :: devlog( '[HELP/DRS] ' . $prompt , $this->pObj->extKey, 1 );
-        }
-      }
       $start      = $start + ( $multiplier * $results_at_a_time ) ;
       $conf_limit = $start . ',' . $results_at_a_time; 
     }
