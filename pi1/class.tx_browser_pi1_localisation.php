71096401df5d7ddb3cdface71d5e861535203a66
dwildt <dwildt@735d13b6-9817-0410-8766-e36946ffe9aa>
2012-05-13 00:21:08 +0000

    3.9.13 +++ query list view
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/browser/trunk@62071 735d13b6-9817-0410-8766-e36946ffe9aa

--- pi1/class.tx_browser_pi1_localisation.php
+++ pi1/class.tx_browser_pi1_localisation.php
@@ -1755,7 +1755,7 @@ class tx_browser_pi1_localisation
       // Load the TCA
     $this->pObj->objZz->loadTCA( $table );
 
-$this->pObj->dev_var_dump( $GLOBALS['TCA'][$table]['columns'] );
+$this->pObj->dev_var_dump( $field, $GLOBALS['TCA'][$table]['columns'] );
     
       // Get and set the l10n_mode
     if( ! isset( $GLOBALS['TCA'][$table]['columns'][$field] ) )
