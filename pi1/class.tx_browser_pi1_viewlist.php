9e84e87f4066a41f1e8177cb8718d579499eb44f
dwildt <dwildt@735d13b6-9817-0410-8766-e36946ffe9aa>
2012-05-13 21:32:44 +0000

    3.9.14 +++ general improvements
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/browser/trunk@62152 735d13b6-9817-0410-8766-e36946ffe9aa

--- pi1/class.tx_browser_pi1_viewlist.php
+++ pi1/class.tx_browser_pi1_viewlist.php
@@ -1172,11 +1172,11 @@ class tx_browser_pi1_viewlist
     $select   = "DISTINCT " . $tableUid . " AS '" . $tableUid . "'";
     $from     = $this->pObj->objSqlInit->statements['listView']['from'];
     $where    = $this->pObj->objSqlInit->statements['listView']['where'];
-//$this->pObj->dev_var_dump( $where );
+$this->pObj->dev_var_dump( $where );
     $where    = $this->pObj->objSqlFun->zz_concatenateWithAnd( $where, $andWhereSysLanguage );
-//$this->pObj->dev_var_dump( $where );
+$this->pObj->dev_var_dump( $where );
     $where    = $this->pObj->objSqlFun->zz_concatenateWithAnd( $where, $andWhereIdList );
-//$this->pObj->dev_var_dump( $where );
+$this->pObj->dev_var_dump( $where );
     if( $this->pObj->objFltr4x->init_aFilterIsSelected( ) )
     {
       $where  = $this->pObj->objSqlFun->zz_concatenateWithAnd( $where, $this->pObj->objFltr4x->andWhereFilter );
