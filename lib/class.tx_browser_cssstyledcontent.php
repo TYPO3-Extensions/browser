beee9a603fe60b7d6afbaa83ea977c3e41328304
dwildt <dwildt@735d13b6-9817-0410-8766-e36946ffe9aa>
2012-05-16 23:33:15 +0000

    3.9.14 +++ Download Module
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/browser/trunk@62367 735d13b6-9817-0410-8766-e36946ffe9aa

--- lib/class.tx_browser_cssstyledcontent.php
+++ lib/class.tx_browser_cssstyledcontent.php
@@ -460,19 +460,16 @@ class tx_browser_cssstyledcontent extends tx_cssstyledcontent_pi1
       if( $table != 'tx_dam' )
       {
         $fileList   = $this->cObj->filelist($filePathConf);
-        list($path) = explode('|', $filePathConf);
       }
       if( $table == 'tx_dam' )
       {
           // Get the list of files from the field
         $fileList = trim($this->cObj->stdWrap($conf['fields.']['files'], $conf['fields.']['files.']));
       }
+      list( $path ) = explode( '|', $filePathConf );
     }
 
       // file path variable isn't set
-      // #37165, 120517, dwildt, 1-
-    //if (empty($filePathConf))
-      // #37165, 120517, dwildt, 1-
     if ( empty( $filePathConf ) )
     {
         // Get the list of files from the field
