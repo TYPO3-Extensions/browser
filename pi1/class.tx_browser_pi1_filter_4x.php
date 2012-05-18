593ad41b4287b1945e7cbd918502e6d6c1e3b7e6
dwildt <dwildt@735d13b6-9817-0410-8766-e36946ffe9aa>
2012-05-18 23:30:47 +0000

    3.9.14 +++ Count hits
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/browser/trunk@62481 735d13b6-9817-0410-8766-e36946ffe9aa

--- pi1/class.tx_browser_pi1_filter_4x.php
+++ pi1/class.tx_browser_pi1_filter_4x.php
@@ -1859,7 +1859,7 @@ class tx_browser_pi1_filter_4x {
     $debugTrailLevel = 1;
     $this->pObj->timeTracking_log( $debugTrailLevel,  'begin' );
 
-$this->pObj-dev_var_dump( $this->pObj->objFlexform->sheet_viewList_count_hits );
+$this->pObj->dev_var_dump( $this->pObj->objFlexform->sheet_viewList_count_hits );
 if( $this->pObj->objFlexform->sheet_viewList_count_hits )
 {
     // 1. step: filter items with one hit at least
