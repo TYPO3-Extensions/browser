3fc6fcae25b8a793329d8a4871feada4523d4157
dwildt <dwildt@735d13b6-9817-0410-8766-e36946ffe9aa>
2012-05-18 13:27:24 +0000

    3.9.14 +++ Bugfix hits first item tree view
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/browser/trunk@62428 735d13b6-9817-0410-8766-e36946ffe9aa

--- pi1/class.tx_browser_pi1_filter_4x.php
+++ pi1/class.tx_browser_pi1_filter_4x.php
@@ -1369,6 +1369,7 @@ class tx_browser_pi1_filter_4x {
     }
       // SWITCH first item
       // Get the COA configuration for the value
+$this->pObj->dev_var_dump( $uid, $bool_displayHits );
 
     $item  = $this->pObj->cObj->cObjGetSingle( $cObj_name, $cObj_conf );
 
