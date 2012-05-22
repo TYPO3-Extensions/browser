d7cbf6a5f738153bc8b779f9f692192058e91106
dwildt <dwildt@735d13b6-9817-0410-8766-e36946ffe9aa>
2012-05-22 18:13:21 +0000

    3.9.18 +++ debugging
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/browser/trunk@62715 735d13b6-9817-0410-8766-e36946ffe9aa

--- pi1/class.tx_browser_pi1_backend.php
+++ pi1/class.tx_browser_pi1_backend.php
@@ -1890,7 +1890,8 @@ class tx_browser_pi1_backend
         // 120515, dwildt, 1-
       //$jquery_ui_label  = $this->locallang[ $arr_jquery_ui['label'] ];
         // 120515, dwildt, 9+
-$this->pObj->dev_var_dump( $this->pObj->typo3Version );
+//$this->pObj->dev_var_dump( $this->pObj->typo3Version );
+var_dump( __METHOD__, __LINE__, $this->pObj->typo3Version );
       switch( true )
       {
         case( $this->pObj->typo3Version < 4005000):
