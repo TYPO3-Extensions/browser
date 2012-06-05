099e5177654b10378954a71fac36aaf198402506
dwildt <dwildt@735d13b6-9817-0410-8766-e36946ffe9aa>
2012-06-05 21:22:07 +0000

    3.9.26 +++ Bugfixes
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/browser/trunk@63153 735d13b6-9817-0410-8766-e36946ffe9aa

--- pi1/class.tx_browser_pi1_template.php
+++ pi1/class.tx_browser_pi1_template.php
@@ -1858,6 +1858,8 @@ class tx_browser_pi1_template
       }
       // imageTitleText
       $markerArray['###IMAGE###'] = $this->pObj->objWrapper->wrapImage($tsImage);
+      // 3.9.26, 120506, dwildt, 1+
+      $markerArray['###' . strtoupper( $handleAs['image'] ) . '###'] = $markerArray['###IMAGE###'];
 
       // 3.9.26, 120506, dwildt, 2+
     if( $b_marker_image )
