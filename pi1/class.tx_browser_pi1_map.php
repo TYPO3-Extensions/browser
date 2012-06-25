f9f15a83c5dcf1eec5e5bf5a47ca05143042ae5c
dwildt <dwildt@735d13b6-9817-0410-8766-e36946ffe9aa>
2012-06-25 09:52:13 +0000

    4.1.0: OpenStreetMap - second step
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/browser/trunk@63827 735d13b6-9817-0410-8766-e36946ffe9aa

--- pi1/class.tx_browser_pi1_map.php
+++ pi1/class.tx_browser_pi1_map.php
@@ -626,7 +626,7 @@ class tx_browser_pi1_map
                                         'point2' => array
                                                     (
                                                       'coors' => array( 9.555442525, 48.933978799 ),
-                                                      'desc'  => 'Punkt2<br />Neue Box und der Inhalt geht &uuml;ber mehrere Zeilen'
+                                                      'desc'  => '<h1>Punkt2<h1><p>Mit HTML Tags umklammert.</p>'
                                                     )
                                       )
                           ),
