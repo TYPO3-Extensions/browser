eb567b980ad880f7542761d3a01626dd087e0876
dwildt <dwildt@735d13b6-9817-0410-8766-e36946ffe9aa>
2012-06-26 23:57:50 +0000

    4.1.0: OpenStreetMap - second step
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/browser/trunk@63972 735d13b6-9817-0410-8766-e36946ffe9aa

--- pi1/class.tx_browser_pi1_map.php
+++ pi1/class.tx_browser_pi1_map.php
@@ -901,17 +901,31 @@ class tx_browser_pi1_map
           // Get the quotient. Example: 360 / 5.625 = 64
         $quotient  = 360 / $maxDistance;
           // Example: ( int ) log( 64 ) / log( 2 ) = 6
-        $zoomLevel = ( ( int ) ( log( $quotient ) / log( 2 ) ) ) + 2;
-        if( $zoomLevel > 18 )
-        {
-          $zoomLevel = 18;
-        }
+        $zoomLevel = ( int ) ( log( $quotient ) / log( 2 ) );
         break;
     }
 //var_dump( __METHOD__, __LINE__, $longitudes, max( $longitudes ), min( $longitudes ),
 //         $latitudes, max( $latitudes ), min( $latitudes ), $maxDistance, $quotient, $zoomLevel );
 //var_dump( __METHOD__, __LINE__, $zoomLevel );
       // Calculate the zoom level
+
+    switch( true )
+    {
+      case( $zoomLevel < 6 ):
+        $zoomLevel = $zoomLevel + 1;
+        break;
+      case( $zoomLevel < 12 ):
+        $zoomLevel = $zoomLevel + 2;
+        break;
+      case( $zoomLevel < 18 ):
+        $zoomLevel = $zoomLevel + 3;
+        break;
+    }
+
+    if( $zoomLevel > 18 )
+    {
+      $zoomLevel = 18;
+    }
 
       // DRS
     if( $this->pObj->b_drs_map )
