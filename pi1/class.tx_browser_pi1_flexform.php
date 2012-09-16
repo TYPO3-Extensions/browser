b6ad9a4b046d57921a984842b86d844ca86400a9
dwildt <dwildt@735d13b6-9817-0410-8766-e36946ffe9aa>
2012-09-16 16:12:05 +0000

    4.1.10 +++ Bugfix: multiple plugins
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/browser/trunk@66120 735d13b6-9817-0410-8766-e36946ffe9aa

--- pi1/class.tx_browser_pi1_flexform.php
+++ pi1/class.tx_browser_pi1_flexform.php
@@ -410,14 +410,14 @@ class tx_browser_pi1_flexform {
     {
         // #40959 4.1.10, 120916, dwildt, -
         // DRS
-$this->pObj->dev_var_dump( $this->pObj->b_drs_warn, $this->pObj->cObj->data['pid'], ( string ) $GLOBALS['TSFE']->id, $this->pObj->cObj->data['pid'] != ( string ) $GLOBALS['TSFE']->id );    
+//$this->pObj->dev_var_dump( $this->pObj->b_drs_warn, $this->pObj->cObj->data['pid'], ( string ) $GLOBALS['TSFE']->id, $this->pObj->cObj->data['pid'] != ( string ) $GLOBALS['TSFE']->id );    
       if( $this->pObj->b_drs_warn ) 
       {
         if( $this->pObj->cObj->data['pid'] != ( string ) $GLOBALS['TSFE']->id )
         {
 $this->pObj->dev_var_dump( $this->pObj->cObj->data['pid'], ( string ) $GLOBALS['TSFE']->id );    
-          $prompt = 'The current plugin ' . $this->pObj->cObj->data['title'] . ' (uid: ' . $this->pObj->cObj->data['title'] . ') ' .
-                    'is not part of the current page (uid ' . $GLOBALS['TSFE']->id . ') but of the page with the uid ' . $this->pObj->cObj->data['pid'] .
+          $prompt = 'The current plugin ' . $this->pObj->cObj->data['header'] . ' (uid: ' . $this->pObj->cObj->data['uid'] . ') ' .
+                    'is not part of the current page (uid ' . $GLOBALS['TSFE']->id . ') but of the page with the uid ' . $this->pObj->cObj->data['pid'] . '. ' .
                     'This will cause trouble in case of multiple plugins!';
           t3lib_div :: devlog( '[WARN/FLEXFORM] ' . $prompt, $this->pObj->extKey, 3 );
           $prompt = 'Please move the plugin to the current page, if you are working with multiple plugins.';
