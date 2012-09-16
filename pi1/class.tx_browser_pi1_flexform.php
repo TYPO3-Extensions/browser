f2418a082dc139c279002b0e37caee02113ade7f
dwildt <dwildt@735d13b6-9817-0410-8766-e36946ffe9aa>
2012-09-16 16:04:36 +0000

    4.1.10 +++ Bugfix: multiple plugins
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/browser/trunk@66117 735d13b6-9817-0410-8766-e36946ffe9aa

--- pi1/class.tx_browser_pi1_flexform.php
+++ pi1/class.tx_browser_pi1_flexform.php
@@ -402,8 +402,6 @@ class tx_browser_pi1_flexform {
     //var_dump($rows);
     // Consolidate the Rows in case of Localisation
 
-    // #40959 4.1.10, 120916, dwildt, -
-$this->pObj->dev_var_dump( $this->pObj->cObj->data['pid'], $GLOBALS['TSFE']->id );    
     //////////////////////////////////////////////////////////////////////
     //
     // RETURN, if we have one plugin on the page only
@@ -416,7 +414,23 @@ $this->pObj->dev_var_dump( $this->pObj->cObj->data['pid'], $GLOBALS['TSFE']->id 
       return;
     }
     // RETURN, if we have one plugin on the page only
-    // #40959 4.1.10, 120916, dwildt, -
+    
+      // #40959 4.1.10, 120916, dwildt, -
+      if( $this->pObj->b_drs_warn ) 
+      {
+        if( $this->pObj->cObj->data['pid'] != ( string ) $GLOBALS['TSFE']->id )
+        {
+$this->pObj->dev_var_dump( $this->pObj->cObj->data['pid'], ( string ) $GLOBALS['TSFE']->id );    
+          $prompt = 'The current plugin ' . $this->pObj->cObj->data['title'] . ' (uid: ' . $this->pObj->cObj->data['title'] . ') ' .
+                    'is not part of the current page (uid ' . $GLOBALS['TSFE']->id . ') but of the page with the uid ' . $this->pObj->cObj->data['pid'] .
+                    'This will cause trouble in case of multiple plugins!';
+          t3lib_div :: devlog( '[WARN/FLEXFORM] ' . $prompt, $this->pObj->extKey, 3 );
+          $prompt = 'Please move the plugin to the current page, if you are working with multiple plugins.';
+          t3lib_div :: devlog( '[HELP/FLEXFORM] ' . $prompt, $this->pObj->extKey, 1 );
+        }
+      }
+    
+      // #40959 4.1.10, 120916, dwildt, -
 
     //////////////////////////////////////////////////////////////////////
     //
