2d31a3ba0117fdc773691ee16d9551976fb3eb25
dwildt <dwildt@735d13b6-9817-0410-8766-e36946ffe9aa>
2012-09-28 21:32:07 +0000

    4.1.18 +++ jsTree jQuery 1.8 Bugfix
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/browser/trunk@66432 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ res/js/jquery/tx_browser_pi1_jstree-4.1.18.js
@@ -0,0 +1,77 @@
+/**
+ *
+ * Copyright (c) 2012 Dirk Wildt
+ * http://wildt.at.die-netzmacher.de/
+ *
+ * Version 3.9.21
+ *
+ * jquery.jstree-x.x.x.js is needed:
+ *   http://docs.jquery.com/Plugins/jstree
+ *
+ * Dual licensed under the MIT and GPL licenses:
+ *   http://www.opensource.org/licenses/mit-license.php
+ *   http://www.gnu.org/licenses/gpl.html
+ */
+
+
+
+$( document ).ready( function( )
+{
+
+  if( $( "###SELECTOR_01###" ).length )
+  {
+    $("###SELECTOR_01###").jstree({
+      "themes" : {
+        "theme" : "###THEME_01###",
+        "dots"  : true,
+        "icons" : true
+      },
+      "plugins" : ["themes", "html_data", "cookies"]
+    });
+  }
+  if( $( "###SELECTOR_02###" ).length )
+  {
+    $("###SELECTOR_02###").jstree({
+      "themes" : {
+        "theme" : "###THEME_02###",
+        "dots"  : true,
+        "icons" : true
+      },
+      "plugins" : ["themes", "html_data", "cookies"]
+    });
+  }
+  if( $( "###SELECTOR_03###" ).length )
+  {
+    $("###SELECTOR_03###").jstree({
+      "themes" : {
+        "theme" : "###THEME_03###",
+        "dots"  : true,
+        "icons" : true
+      },
+      "plugins" : ["themes", "html_data", "cookies"]
+    });
+  }
+  if( $( "###SELECTOR_04###" ).length )
+  {
+    $("###SELECTOR_04###").jstree({
+      "themes" : {
+        "theme" : "###THEME_04###",
+        "dots"  : true,
+        "icons" : true
+      },
+      "plugins" : ["themes", "html_data", "cookies"]
+    });
+  }
+  if( $( "###SELECTOR_05###" ).length )
+  {
+    $("###SELECTOR_05###").jstree({
+      "themes" : {
+        "theme" : "###THEME_05###",
+        "dots"  : true,
+        "icons" : true
+      },
+      "plugins" : ["themes", "html_data", "cookies"]
+    });
+  }
+
+});
