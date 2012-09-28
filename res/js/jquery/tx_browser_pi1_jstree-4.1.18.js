26247fd915b17b2e892b6723a02309f019fa0af2
dwildt <dwildt@735d13b6-9817-0410-8766-e36946ffe9aa>
2012-09-28 21:46:46 +0000

    4.1.18 +++ jsTree jQuery 1.8 Bugfix
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/browser/trunk@66434 735d13b6-9817-0410-8766-e36946ffe9aa

--- res/js/jquery/tx_browser_pi1_jstree-4.1.18.js
+++ res/js/jquery/tx_browser_pi1_jstree-4.1.18.js
@@ -3,7 +3,7 @@
  * Copyright (c) 2012 Dirk Wildt
  * http://wildt.at.die-netzmacher.de/
  *
- * Version 3.9.21
+ * Version 4.1.18
  *
  * jquery.jstree-x.x.x.js is needed:
  *   http://docs.jquery.com/Plugins/jstree
@@ -29,49 +29,49 @@ $( document ).ready( function( )
       "plugins" : ["themes", "html_data", "cookies"]
     });
   }
-  if( $( "###SELECTOR_02###" ).length )
-  {
-    $("###SELECTOR_02###").jstree({
-      "themes" : {
-        "theme" : "###THEME_02###",
-        "dots"  : true,
-        "icons" : true
-      },
-      "plugins" : ["themes", "html_data", "cookies"]
-    });
-  }
-  if( $( "###SELECTOR_03###" ).length )
-  {
-    $("###SELECTOR_03###").jstree({
-      "themes" : {
-        "theme" : "###THEME_03###",
-        "dots"  : true,
-        "icons" : true
-      },
-      "plugins" : ["themes", "html_data", "cookies"]
-    });
-  }
-  if( $( "###SELECTOR_04###" ).length )
-  {
-    $("###SELECTOR_04###").jstree({
-      "themes" : {
-        "theme" : "###THEME_04###",
-        "dots"  : true,
-        "icons" : true
-      },
-      "plugins" : ["themes", "html_data", "cookies"]
-    });
-  }
-  if( $( "###SELECTOR_05###" ).length )
-  {
-    $("###SELECTOR_05###").jstree({
-      "themes" : {
-        "theme" : "###THEME_05###",
-        "dots"  : true,
-        "icons" : true
-      },
-      "plugins" : ["themes", "html_data", "cookies"]
-    });
-  }
+//  if( $( "###SELECTOR_02###" ).length )
+//  {
+//    $("###SELECTOR_02###").jstree({
+//      "themes" : {
+//        "theme" : "###THEME_02###",
+//        "dots"  : true,
+//        "icons" : true
+//      },
+//      "plugins" : ["themes", "html_data", "cookies"]
+//    });
+//  }
+//  if( $( "###SELECTOR_03###" ).length )
+//  {
+//    $("###SELECTOR_03###").jstree({
+//      "themes" : {
+//        "theme" : "###THEME_03###",
+//        "dots"  : true,
+//        "icons" : true
+//      },
+//      "plugins" : ["themes", "html_data", "cookies"]
+//    });
+//  }
+//  if( $( "###SELECTOR_04###" ).length )
+//  {
+//    $("###SELECTOR_04###").jstree({
+//      "themes" : {
+//        "theme" : "###THEME_04###",
+//        "dots"  : true,
+//        "icons" : true
+//      },
+//      "plugins" : ["themes", "html_data", "cookies"]
+//    });
+//  }
+//  if( $( "###SELECTOR_05###" ).length )
+//  {
+//    $("###SELECTOR_05###").jstree({
+//      "themes" : {
+//        "theme" : "###THEME_05###",
+//        "dots"  : true,
+//        "icons" : true
+//      },
+//      "plugins" : ["themes", "html_data", "cookies"]
+//    });
+//  }
 
 });
