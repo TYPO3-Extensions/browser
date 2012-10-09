5a4f0613c783346cd8eb0ecbc01e0afd4a5b7abc
dwildt <dwildt@735d13b6-9817-0410-8766-e36946ffe9aa>
2012-10-09 23:07:00 +0000

    4.1.21 +++ #41776: Make filter 4.x independent of filter 3.x
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/browser/trunk@66779 735d13b6-9817-0410-8766-e36946ffe9aa

--- pi1/class.tx_browser_pi1_filter_4x.php
+++ pi1/class.tx_browser_pi1_filter_4x.php
@@ -2688,16 +2688,16 @@ class tx_browser_pi1_filter_4x {
     $orderBy  = $this->sql_orderBy( );
     $limit    = $this->sql_limit( );
 
-//    $query  = $GLOBALS['TYPO3_DB']->SELECTquery
-//              (
-//                $select,
-//                $from,
-//                $where,
-//                $groupBy,
-//                $orderBy,
-//                $limit
-//              );
-//$this->pObj->dev_var_dump( $query );
+    $query  = $GLOBALS['TYPO3_DB']->SELECTquery
+              (
+                $select,
+                $from,
+                $where,
+                $groupBy,
+                $orderBy,
+                $limit
+              );
+$this->pObj->dev_var_dump( $query );
 
       // Execute query
     $arr_return = $this->pObj->objSqlFun->exec_SELECTquery
