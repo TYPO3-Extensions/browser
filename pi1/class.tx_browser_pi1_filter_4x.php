62a2087399fd9dd6641efe47eb9ce9a90b833877
dwildt <dwildt@735d13b6-9817-0410-8766-e36946ffe9aa>
2012-10-09 20:36:35 +0000

    4.1.21 +++ #41776: Make filter 4.x independent of filter 3.x
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/browser/trunk@66750 735d13b6-9817-0410-8766-e36946ffe9aa

--- pi1/class.tx_browser_pi1_filter_4x.php
+++ pi1/class.tx_browser_pi1_filter_4x.php
@@ -5808,7 +5808,7 @@ class tx_browser_pi1_filter_4x {
         // SWITCH field
     }
       // LOOP all fields of current filter / tableField
-
+$this->pObj->dev_var_dump( $firstItem );
       // Add first item to the rows of the current filter
     $this->rows = $firstItem + $this->rows;
 
