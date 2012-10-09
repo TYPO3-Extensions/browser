fc85d3a16c89feeaa47f6fd204d78f8fa9f8050b
dwildt <dwildt@735d13b6-9817-0410-8766-e36946ffe9aa>
2012-10-09 20:55:09 +0000

    4.1.21 +++ #41776: Make filter 4.x independent of filter 3.x
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/browser/trunk@66757 735d13b6-9817-0410-8766-e36946ffe9aa

--- pi1/class.tx_browser_pi1_cal.php
+++ pi1/class.tx_browser_pi1_cal.php
@@ -2387,7 +2387,7 @@ class tx_browser_pi1_cal
       // Wrap items, recalculate hits
 
     $arr_fields = $arr_ts['area.'][$str_case . '.']['options.']['fields.'];
-$this->pObj->dev_var_dump(__METHOD__ . ' (' . __LINE__ . ')', $table, $arr_fields );
+$this->pObj->dev_var_dump(__METHOD__ . ' (' . __LINE__ . ')', $table, $arr_values, $arr_fields );
     foreach( ( array) $arr_fields as $keyWiDot => $arr_string )
     {
 $this->pObj->dev_var_dump(__METHOD__ . ' (' . __LINE__ . ')' );
