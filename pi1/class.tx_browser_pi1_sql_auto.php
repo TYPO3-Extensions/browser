e2bdafb9513b9f97810d16779310e3fbe106e601
dwildt <dwildt@735d13b6-9817-0410-8766-e36946ffe9aa>
2012-10-10 09:51:38 +0000

    4.1.21 +++ #41776: Make filter 4.x independent of filter 3.x
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/browser/trunk@66804 735d13b6-9817-0410-8766-e36946ffe9aa

--- pi1/class.tx_browser_pi1_sql_auto.php
+++ pi1/class.tx_browser_pi1_sql_auto.php
@@ -713,6 +713,7 @@ class tx_browser_pi1_sql_auto
       //////////////////////////////////////////////////////////////////////////
       //
       // Is there an andWhere statement from the filter class?
+$this->pObj->dev_var_dump( 'X' );
     if ( is_array( $this->pObj->arr_andWhereFilter ) )
     {
       $str_andFilter  = implode( " AND ", $this->pObj->arr_andWhereFilter );
