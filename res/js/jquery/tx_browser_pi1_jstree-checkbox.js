2ec10f8e2205e9794aca330d6869e279bb5c606b
dwildt <dwildt@735d13b6-9817-0410-8766-e36946ffe9aa>
2012-10-10 23:56:44 +0000

    4.1.21 +++ #41753: Treeview as checkbox
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/browser/trunk@66892 735d13b6-9817-0410-8766-e36946ffe9aa

--- res/js/jquery/tx_browser_pi1_jstree-checkbox.js
+++ res/js/jquery/tx_browser_pi1_jstree-checkbox.js
@@ -8,27 +8,24 @@
 
 
 
-  $( document ).ready( function( )
+$( document ).ready( function( )
+{
+  if( $( "###SELECTOR_01###" ).length )
   {
-$(function () {
-
-    if( $( "###SELECTOR_01###" ).length )
-    {
-      $("###SELECTOR_01###").jstree({
-        "themes" : {
-          "theme" : "###THEME_01###",
-          "dots"  : ###DOTS_01###,
-          "icons" : ###ICONS_01###
-        },
-        "checkbox" : {
-          "real_checkboxes" : true
-        },
-        "plugins" : ["themes", "html_data", "checkbox", "sort", "ui", "cookies"]
-      })
-      .bind("select_node.jstree", function (event, data) { 
-        // `data.rslt.obj` is the jquery extended node that was clicked
-        alert(data.rslt.obj.attr("id"));
-      })
-    }
-}
-  });
+    $("###SELECTOR_01###").jstree({
+      "themes" : {
+        "theme" : "###THEME_01###",
+        "dots"  : ###DOTS_01###,
+        "icons" : ###ICONS_01###
+      },
+      "checkbox" : {
+        "real_checkboxes" : true
+      },
+      "plugins" : ["themes", "html_data", "checkbox", "sort", "ui", "cookies"]
+    })
+    .bind("select_node.jstree", function (event, data) { 
+      // `data.rslt.obj` is the jquery extended node that was clicked
+      alert(data.rslt.obj.attr("id"));
+    })
+  }
+});
