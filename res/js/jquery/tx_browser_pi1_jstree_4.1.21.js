f40e3f9c79b3481a9f4c9bfdb877f707453c0284
dwildt <dwildt@735d13b6-9817-0410-8766-e36946ffe9aa>
2012-10-11 21:51:51 +0000

    4.1.21 +++ #41753: Treeview as checkbox
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/browser/trunk@66974 735d13b6-9817-0410-8766-e36946ffe9aa

--- res/js/jquery/tx_browser_pi1_jstree_4.1.21.js
+++ res/js/jquery/tx_browser_pi1_jstree_4.1.21.js
@@ -97,3 +97,31 @@ $( document ).ready( function( )
   }
 
 });
+
+function generateHiddenFieldsForTree( ) 
+{
+  var checked_ids = [];
+  var name  = "tx_browser_pi1[tx_greencars_manufacturer.title][]";
+
+    // Append an input field for each selected <li>-item to the current form
+  $( "#tx_greencars_manufacturer_title" ).jstree( "get_checked" , null, true ).each(function( )
+  {
+      // Get current record uid
+    var thisId         = this.id;
+    var thisIdSplitted = thisId.split( "_" );
+    var recordUid      = thisIdSplitted[ thisIdSplitted.length - 1 ];
+
+     // Append an input field with the record uid
+    if( recordUid )
+    {
+      $( "form" ).append('<input type="hidden" name="' + name + '" value="' + recordUid + '" />');
+    }
+  });
+
+}
+$( function ( ) {
+  $( "form" ).submit( function ( )
+  {
+    generateHiddenFieldsForTree( );
+  });
+});
