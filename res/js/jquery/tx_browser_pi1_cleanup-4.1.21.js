45cc3ef643d9951d7f247ec6cca85b81ac6cac4e
dwildt <dwildt@735d13b6-9817-0410-8766-e36946ffe9aa>
2012-10-11 12:59:32 +0000

    4.1.21 +++ #41753: Treeview as checkbox
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/browser/trunk@66921 735d13b6-9817-0410-8766-e36946ffe9aa

--- res/js/jquery/tx_browser_pi1_cleanup-4.1.21.js
+++ res/js/jquery/tx_browser_pi1_cleanup-4.1.21.js
@@ -3,7 +3,7 @@
  * Copyright (c) 2012 - Dirk Wildt
  * http://wildt.at.die-netzmacher.de/
  *
- * @version 0.0.3
+ * @version 4.1.21
  *
  * Dual licensed under the MIT and GPL licenses:
  *   http://www.opensource.org/licenses/mit-license.php
@@ -41,23 +41,6 @@ function cleanup_afterAJAXrequest( )
     //
     // jQuery plugin jstree
 
-  try {
-    if( $( "###SELECTOR_01###" ).length )
-    {
-      $("###SELECTOR_01###").jstree({
-        "themes" : {
-          "theme" : "###THEME_01###",
-          "dots"  : ###DOTS_01###,
-          "icons" : ###ICONS_01###
-        },
-        "plugins" : ["themes", "html_data", "cookies"]
-      });
-    }
-  }
-  catch( err )
-  {
-    // jQuery plugin jstree isn't included. Don't worry!
-  }
   try {
     if( $( "###SELECTOR_01###" ).length )
     {
@@ -117,6 +100,23 @@ function cleanup_afterAJAXrequest( )
           "theme" : "###THEME_04###",
           "dots"  : ###DOTS_04###,
           "icons" : ###ICONS_04###
+        },
+        "plugins" : ["themes", "html_data", "cookies"]
+      });
+    }
+  }
+  catch( err )
+  {
+    // jQuery plugin jstree isn't included. Don't worry!
+  }
+  try {
+    if( $( "###SELECTOR_05###" ).length )
+    {
+      $("###SELECTOR_05###").jstree({
+        "themes" : {
+          "theme" : "###THEME_05###",
+          "dots"  : ###DOTS_05###,
+          "icons" : ###ICONS_05###
         },
         "plugins" : ["themes", "html_data", "cookies"]
       });
