782943fd9a42410dee7ffac0ea532bc37233137a
dwildt <dwildt@735d13b6-9817-0410-8766-e36946ffe9aa>
2012-10-18 14:52:31 +0000

    4.1.22 - #42127, #42128, #42129
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/browser/trunk@67292 735d13b6-9817-0410-8766-e36946ffe9aa

--- res/js/map/oxMapConfig_0.0.9_custom.js
+++ res/js/map/oxMapConfig_0.0.9_custom.js
@@ -21,6 +21,33 @@ var oxMapConfig = {
                       type:    'Mapnik',                                 //    Mapnik
                       name:    'OpenStreetMap (Mapnik)',                 //    OpenStreetMap (Mapnik)
                       center:  [ 11.322578,50.98098 ],                   //    setCenter coordinates [ longitude , latitude ]  longitude = Y, latitude = X
+                      custom:    {
+                        type: 'customBBOX',                            //    custom layer type
+                        content: {
+                          sort : 'image',
+                          size: {                                    //    image size in pixel units
+                            w: 620,
+                            h: 715    
+                          },
+                          bounds: {                                  //    bounding box coordinates for the restricted extent
+                            w: 11.29976,
+                            s: 50.95955,
+                            e: 11.35304,
+                            n: 50.99803
+                          },
+                          png:{                                      //    by default PNG image or for browser, that don't support SVG
+                            0: 'img/ci_map_v2.level0.png',
+                            1: 'img/ci_map_v2.level1.png',
+                            2: 'img/ci_map_v2.level2.png'
+                          },
+                          svg: {                                     //    modern browser support SVG, better image quality in maps !!
+                            0: 'img/ci_map_v2.level0.svg',
+                            1: 'img/ci_map_v2.level1.svg',
+                            2: 'img/ci_map_v2.level1.svg'
+                          }
+                        },
+                        numZoomLevels: 3
+                      },
                       zoomStartLevel: 14                                 //    integer (0 - 19) set zoom level
                     }
 };
