2e33df5d2eaf5a20e353a1936fc675c02071b23d
dwildt <dwildt@735d13b6-9817-0410-8766-e36946ffe9aa>
2013-01-04 16:25:29 +0000

    4.4.0 +++ #44313
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/browser/trunk@69431 735d13b6-9817-0410-8766-e36946ffe9aa

--- pi1/class.tx_browser_pi1_cal.php
+++ pi1/class.tx_browser_pi1_cal.php
@@ -753,7 +753,7 @@ class tx_browser_pi1_cal
       // Get fields and set marker
 
 // #44313, 130104, dwildt
-$this->pObj->dev_var_dump( $GLOBALS['_GET'] );
+$this->pObj->dev_var_dump( $GLOBALS['_GET'], $this->markerArray );
     
 
 
