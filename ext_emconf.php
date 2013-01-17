2cbc254eaa2b21b9714cd38fa0512faea1d9ec46
dwildt <dwildt@735d13b6-9817-0410-8766-e36946ffe9aa>
2013-01-17 23:31:23 +0000

    4.4.3 - Update manual
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/browser/trunk@70148 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -34,7 +34,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'CGLcompliance_note' => '',
 	'constraints' => array(
 		'depends' => array(
-			'browser_manual_en' => '-4.4.0',
+			'browser_manual_en' => '-4.4.3',
 		),
 		'conflicts' => array(
 			'be_tablefilter' => '',
