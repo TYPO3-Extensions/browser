7908c6c24d8a2a3414676c4b9af9ca4e1eae46d6
dwildt <dwildt@735d13b6-9817-0410-8766-e36946ffe9aa>
2013-02-12 21:24:01 +0000

    4.4.7 +++ #45422
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/browser/trunk@71147 735d13b6-9817-0410-8766-e36946ffe9aa

--- pi1/class.tx_browser_pi1_filter_4x.php
+++ pi1/class.tx_browser_pi1_filter_4x.php
@@ -790,6 +790,7 @@ $this->pObj->dev_var_dump(
   $this->pObj->objFlexform->sheet_extend_cal_field_start,  
   $this->pObj->objFlexform->sheet_extend_cal_field_end,  
   $str_andWhere,
+  date('Y-m-d H:i:s', $from),
   date('Y-m-d H:i:s', $to)
 );
     return $str_andWhere;
