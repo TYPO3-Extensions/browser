9cb02ed02d201c236b7671bcf60e56abdca60a10
dwildt <dwildt@735d13b6-9817-0410-8766-e36946ffe9aa>
2013-02-27 14:13:03 +0000

    3.9.15 +++ #45888
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/browser/branches/3.9@71668 735d13b6-9817-0410-8766-e36946ffe9aa

--- pi1/class.tx_browser_pi1_ttcontainer.php
+++ pi1/class.tx_browser_pi1_ttcontainer.php
@@ -503,6 +503,8 @@ class tx_browser_pi1_ttcontainer
     $arr_marker_values  = $this->get_marker_ordered($arr_marker_values);
     // Oder the marker array
     $arr_return         = $this->get_wrapped_marker($arr_ttc_values, $arr_marker_values, $str_ttc_type);
+// #45888
+var_dump( $arr_return );    
     // Get the template as finished HTML code in $arr_return['data']['template']
 
 
