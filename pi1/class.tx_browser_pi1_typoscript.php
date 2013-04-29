d48132573e380fa5e2e78b560653df504ea0eb23
dwildt <dwildt@735d13b6-9817-0410-8766-e36946ffe9aa>
2013-04-29 14:32:16 +0000

    4.5.6 +++ #47680
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/browser/trunk@74973 735d13b6-9817-0410-8766-e36946ffe9aa

--- pi1/class.tx_browser_pi1_typoscript.php
+++ pi1/class.tx_browser_pi1_typoscript.php
@@ -1029,13 +1029,10 @@ $this->pObj->dev_var_dump( $arr_addToSelect );
       // RETURN : $str_queryPart is empty
 
     $arrCsv     = explode( ',', $str_queryPart );
-$this->pObj->dev_var_dump( $key_queryPart, $str_queryPart, $arrCsv );
     $arrCsv     = $this->pObj->objSqlFun_3x->clean_up_as_and_alias( $arrCsv );
-//$this->pObj->dev_var_dump( $arrCsv );
     $arrTmp[0]  = $arrCsv;
     $arrTmp     = $this->pObj->objSqlFun_3x->replace_tablealias( $arrTmp );
     $arrCsv     = $arrTmp[0];
-//$this->pObj->dev_var_dump( $arrCsv );
     
       // LOOP each query part
     foreach( ( array ) $arrCsv as $tableField )
