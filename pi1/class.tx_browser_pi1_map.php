e3f4249cad940a76ab07cb7aca5814e173525a1e
dwildt <dwildt@735d13b6-9817-0410-8766-e36946ffe9aa>
2013-05-09 11:32:05 +0000

    4.5.6 +++ #47632
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/browser/trunk@75288 735d13b6-9817-0410-8766-e36946ffe9aa

--- pi1/class.tx_browser_pi1_map.php
+++ pi1/class.tx_browser_pi1_map.php
@@ -2266,6 +2266,28 @@ if( $this->pObj->b_drs_todo )
                     , 'prompt' => null
                   );
 
+$this->pObj->dev_var_dump( $this->pObj->rows );
+    
+//    $prompt = '<h1 style="color:red;">
+//              ' . $this->pObj->pi_getLL( 'error_readlog_h1' ) . '
+//              </h1>
+//              <p style="color:red;font-weight:bold;">
+//              ' . $this->pObj->pi_getLL( 'error_template_map_no' ) . '
+//              </p>';
+    $prompt = '<h1 style="color:red;">
+                ERROR with Map +Route
+              </h1>
+              <p style="color:red;font-weight:bold;">
+                Sorry, but there is an undefined error with Map +Route
+              </p>
+              <p style="color:red;font-weight:bold;">
+                Method: ' . __METHOD__ . ' at line ' . __LINE__ . '
+              </p>
+              ';
+
+    $arr_return['error']  = true;
+    $arr_return['prompt'] = $prompt;
+
     return $arr_return;
   }
 
