dc9980359c796028c56126f4a743a26e7a739e4c
dwildt <dwildt@735d13b6-9817-0410-8766-e36946ffe9aa>
2013-06-01 21:46:28 +0000

    4.5.7 - #47630
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/browser/trunk@75933 735d13b6-9817-0410-8766-e36946ffe9aa

--- pi1/class.tx_browser_pi1_map.php
+++ pi1/class.tx_browser_pi1_map.php
@@ -1709,7 +1709,7 @@ if( $this->pObj->b_drs_todo )
         break;
     }
       // #47631, #i0007, dwildt, 18+
-//$this->pObj->dev_var_dump( $this->pObj->rows );
+$this->pObj->dev_var_dump( $this->pObj->rows );
     foreach( $this->pObj->rows as $row )
     {
         // IF there are more than one category
