465bd01f3332073d0f8f1d3579a7afe30812d69f
dwildt <dwildt@735d13b6-9817-0410-8766-e36946ffe9aa>
2013-06-01 14:15:41 +0000

    4.5.7 - #47630
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/browser/trunk@75897 735d13b6-9817-0410-8766-e36946ffe9aa

--- pi1/class.tx_browser_pi1_map.php
+++ pi1/class.tx_browser_pi1_map.php
@@ -2539,7 +2539,7 @@ if( $this->pObj->b_drs_todo )
                 $marker, 
                 $this->renderMapRouteMarkerByPath( )
               );
-$this->pObj->dev_var_dump( $marker );
+//$this->pObj->dev_var_dump( $marker );
 
       // DRS
     if( $this->pObj->b_drs_map )
