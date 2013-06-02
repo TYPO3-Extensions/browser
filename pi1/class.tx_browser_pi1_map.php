630944a838d2561b5a0f83094c6d9efae2b5b9e4
dwildt <dwildt@735d13b6-9817-0410-8766-e36946ffe9aa>
2013-06-02 15:09:57 +0000

    4.5.7 - #47630
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/browser/trunk@76004 735d13b6-9817-0410-8766-e36946ffe9aa

--- pi1/class.tx_browser_pi1_map.php
+++ pi1/class.tx_browser_pi1_map.php
@@ -1740,7 +1740,7 @@ if( $this->pObj->b_drs_todo )
         // Get mapMarkers, lats and lons
       $arr_result     = $this->renderMapMarkerPointsPoint( $row, $arrLabels, $arrCategoriesFlipped );
       $mapMarkers[ ]  = $arr_result['data']['mapMarkers'];
-      $lats[ ]        = $arr_result['data']['lats'];
+      $lats        = array_merge( $lats, $arr_result['data']['lats'] );
       $lons[ ]        = $arr_result['data']['lons'];
       unset( $arr_result );
         // Get mapMarkers, lats and lons
