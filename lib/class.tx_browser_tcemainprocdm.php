4c7d450475cbb2a6adb7bc726c7804038a999159
dwildt <dwildt@735d13b6-9817-0410-8766-e36946ffe9aa>
2013-06-03 14:06:31 +0000

    4.5.7 - #47630
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/browser/trunk@76102 735d13b6-9817-0410-8766-e36946ffe9aa

--- lib/class.tx_browser_tcemainprocdm.php
+++ lib/class.tx_browser_tcemainprocdm.php
@@ -57,9 +57,10 @@ class tx_browser_tcemainprocdm
     if( $status == 'update' && $table == 'pages' )
     {
       $fieldArray[ 'hidden' ] = 1;
+      
       $table      = $table;
       $recuid     = $id;
-      $action     = 5; // Action number: 0=No category, 1=new record, 2=update record, 3= delete record, 4= move record, 5= Check/evaluate
+      $action     = 3; // Action number: 0=No category, 1=new record, 2=update record, 3= delete record, 4= move record, 5= Check/evaluate
       $recpid     = $id; 
       $error      = 1;  // 0 = message, 1 = error, 2 = System Error, 3 = security notice 
       $details    = 'prompt';    
