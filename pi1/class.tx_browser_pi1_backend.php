135aa91665017963b1f5caa00588653264ae5749
dwildt <dwildt@735d13b6-9817-0410-8766-e36946ffe9aa>
2013-07-20 16:09:44 +0000

    4.5.11 +++ #50222
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/browser/trunk@78026 735d13b6-9817-0410-8766-e36946ffe9aa

--- pi1/class.tx_browser_pi1_backend.php
+++ pi1/class.tx_browser_pi1_backend.php
@@ -2314,7 +2314,7 @@ class tx_browser_pi1_backend
       switch( true )
       {
         // #49495, 130702, dwildt, 1-
-        case( $this->typo3Version < 6000000 ):
+        case( $this->typo3Version < 4006000 ):
           $jquery_ui_label  = $this->locallang[ $arr_jquery_ui['label'] ];
 //var_dump( __METHOD__, __LINE__, $jquery_ui_label );
           break;
