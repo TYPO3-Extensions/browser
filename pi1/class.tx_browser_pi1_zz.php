7721a0390c418d43ce772df4d20ca882885b048d
dwildt <dwildt@735d13b6-9817-0410-8766-e36946ffe9aa>
2013-07-20 10:16:39 +0000

    4.5.11 +++ #50214
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/browser/trunk@77981 735d13b6-9817-0410-8766-e36946ffe9aa

--- pi1/class.tx_browser_pi1_zz.php
+++ pi1/class.tx_browser_pi1_zz.php
@@ -1937,7 +1937,7 @@
     $tableField     = $this->pObj->objSqlFun_3x->get_sql_alias_behind($tableField);
     $arrColumns[0]  = $tableField;
   // #50214, 130720, dwildt, 1+
-$this->pObj->dev_var_dump( $this->pObj->arrLinkToSingle );
+$this->pObj->dev_var_dump( $arrColumns );
     $arrColumns     = $this->pObj->objSqlFun_3x->replace_tablealias($arrColumns);
     $tableField     = $arrColumns[0];
 
