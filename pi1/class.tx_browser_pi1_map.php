ebef51872031941a393bd29b6fba8e00c4ce2f33
dwildt <dwildt@735d13b6-9817-0410-8766-e36946ffe9aa>
2013-09-07 23:14:52 +0000

    4.5.15 +++ #i0027
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/browser/trunk@79633 735d13b6-9817-0410-8766-e36946ffe9aa

--- pi1/class.tx_browser_pi1_map.php
+++ pi1/class.tx_browser_pi1_map.php
@@ -1984,7 +1984,7 @@ class tx_browser_pi1_map
 
       // Get category properties
     $catValues  = $this->renderMapMarkerPointsPointProperties( $row );
-$this->pObj->dev_var_dump( $row, $catValues, $arrLabels );
+//$this->pObj->dev_var_dump( $row, $catValues, $arrLabels );
 
       // FOREACH category title
     foreach( $catValues[ 'catTitles' ] as $key => $catTitle )
