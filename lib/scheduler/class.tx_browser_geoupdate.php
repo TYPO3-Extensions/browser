99cd303c5a9d6563e44d5cea9b0282fe680027d0
dwildt <dwildt@735d13b6-9817-0410-8766-e36946ffe9aa>
2013-09-08 15:18:24 +0000

    4.5.15 +++ #i0027
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/browser/trunk@79668 735d13b6-9817-0410-8766-e36946ffe9aa

--- lib/scheduler/class.tx_browser_geoupdate.php
+++ lib/scheduler/class.tx_browser_geoupdate.php
@@ -2077,18 +2077,18 @@ table     : ' . $this->browser_table;
         break;
     }
     
-    $flashMessage = t3lib_div::makeInstance( 't3lib_FlashMessage', $fmPrompt, $fmHeader, $fmStatus );
-    t3lib_FlashMessageQueue::addMessage( $flashMessage );    
-
-      // RETURN : Don't prompt to syslog
-    if( $logStatus <= 0 )
+    $GLOBALS[ 'BE_USER' ]->writelog( $type, $action, $logStatus, $details_nr, $details, $data, $table, $recuid, $recpid, $event_pid, $NEWid );
+    
+      // RETURN : Don't prompt to the backend
+    if( $status < 0 )
     {
       return;
     }
-      // RETURN : Don't prompt to syslog
+      // RETURN : Don't prompt to the backend
 
-    $GLOBALS[ 'BE_USER' ]->writelog( $type, $action, $logStatus, $details_nr, $details, $data, $table, $recuid, $recpid, $event_pid, $NEWid );
-    
+    $flashMessage = t3lib_div::makeInstance( 't3lib_FlashMessage', $fmPrompt, $fmHeader, $fmStatus );
+    t3lib_FlashMessageQueue::addMessage( $flashMessage );    
+
   }
 
 
