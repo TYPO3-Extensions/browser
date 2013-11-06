4ca4ceff91e7f581f305f43f53cacb24ec5d25e1
dwildt <dwildt@735d13b6-9817-0410-8766-e36946ffe9aa>
2013-11-06 23:51:45 +0000

    4.8.0 +++ #53397
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/browser/trunk@81304 735d13b6-9817-0410-8766-e36946ffe9aa

--- lib/class.tx_browser_cssstyledcontent.php
+++ lib/class.tx_browser_cssstyledcontent.php
@@ -130,7 +130,7 @@ echo $content . PHP_EOL;
                         $this->helper_init_drs( );
                         $this->cObjDataSet( );
 
-var_export( $this->cObj->data['pi_flexform'], true ) . PHP_EOL;
+var_export( $this->cObj->data['pi_flexform'], false ) . PHP_EOL;
 				// get flexform values
 			$caption = trim(htmlspecialchars($this->pi_getFFvalue($this->cObj->data['pi_flexform'], 'acctables_caption')));
 			$useTfoot = trim($this->pi_getFFvalue($this->cObj->data['pi_flexform'], 'acctables_tfoot'));
