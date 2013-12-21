a6657a971acc439c73bec4f443a0b7d76da94529
dwildt <dwildt@735d13b6-9817-0410-8766-e36946ffe9aa>
2013-12-21 15:06:56 +0000

    4.8.5 +++ #54548
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/browser/trunk@82131 735d13b6-9817-0410-8766-e36946ffe9aa

--- pi1/class.tx_browser_pi1_map.php
+++ pi1/class.tx_browser_pi1_map.php
@@ -322,7 +322,7 @@ class tx_browser_pi1_map
 //}
 
       // FOREACH row
-//$this->pObj->dev_var_dump( $this->pObj->rows );
+$this->pObj->dev_var_dump( $this->pObj->rows );
     foreach( $this->pObj->rows as $row )
     {
 //$this->pObj->dev_var_dump( $fieldForLabel, array_keys( $row ) );
