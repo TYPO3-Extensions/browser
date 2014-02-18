659e57a9dd119e2bcca59fcef32ee06aa38065bb
dwildt <dwildt@735d13b6-9817-0410-8766-e36946ffe9aa>
2014-02-18 18:35:11 +0000

    4.8.7 +++ #56088
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/browser/trunk@83846 735d13b6-9817-0410-8766-e36946ffe9aa

--- pi1/class.tx_browser_pi1_viewlist.php
+++ pi1/class.tx_browser_pi1_viewlist.php
@@ -270,6 +270,7 @@ class tx_browser_pi1_viewlist
       // Set rows
     $this->rows_fromSqlRes( $res );
     $rows = $this->pObj->rows;
+$this->pObj->dev_var_dump( $rows );
 
       // DRS
     if( $this->pObj->b_drs_devTodo )
@@ -280,12 +281,14 @@ class tx_browser_pi1_viewlist
       // DRS
 
       // Consolidate localisation
+$this->pObj->dev_var_dump( $rows );
     $rows = $this->rows_consolidateLL( $rows );
       // Consolidate children
     $rows = $this->rows_consolidateChildren( $rows );
     $this->pObj->rows = $rows;
 
       // Implement the hook rows_filter_values
+$this->pObj->dev_var_dump( $rows );
     $this->hook_afterConsolidatetRows( );
     $rows = $this->pObj->rows;
 
@@ -297,6 +300,7 @@ class tx_browser_pi1_viewlist
     }
 
       // Ordering the children rows
+$this->pObj->dev_var_dump( $rows );
     $rows = $this->pObj->objMultisort->multisort_mm_children( $rows );
     $this->pObj->rows = $rows;
 
@@ -309,6 +313,7 @@ class tx_browser_pi1_viewlist
       // DRS - :TODO:
 
       // Delete fields, which were added whily runtime
+$this->pObj->dev_var_dump( $rows );
     $arr_return = $this->pObj->objSqlFun_3x->rows_with_cleaned_up_fields( $rows );
     $rows       = $arr_return['data']['rows'];
     unset($arr_return);
