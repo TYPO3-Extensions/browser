fa2512479d0ae40a61b8a05532fa86c4f251fb55
dwildt <dwildt@735d13b6-9817-0410-8766-e36946ffe9aa>
2014-10-07 20:39:50 +0000

    6.0.0 +++ #62044
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/browser/trunk@86519 735d13b6-9817-0410-8766-e36946ffe9aa

--- static/Foundation/Framework/page/setup.ts
+++ static/Foundation/Framework/page/setup.ts
@@ -1,5 +1,10 @@
 page {
 
+  meta {
+      // Foundation
+    viewport = width=device-width, initial-scale=1.0
+  }
+
   includeCSS {
     browserNormalize  = {$plugin.tx_browser_pi1.frameworks.foundation.framework.page.includeCSS.normalize}
     browserFoundation = {$plugin.tx_browser_pi1.frameworks.foundation.framework.page.includeCSS.foundation}
