f63e6c057f7803a311d0a742bd0a8d63a847ba6e
dwildt <dwildt@735d13b6-9817-0410-8766-e36946ffe9aa>
2014-11-01 19:30:12 +0000

    6.0.2 +++ #62615
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/browser/trunk@86822 735d13b6-9817-0410-8766-e36946ffe9aa

--- res/js/tx_browser_pi1_ajax_languages.js
+++ res/js/tx_browser_pi1_ajax_languages.js
@@ -15,10 +15,10 @@ var lang_returnToList = new Array;
 var lang_ajaxErrorMsg = new Array;
 
 lang_returnToList.en = 'Show list again';
-lang_ajaxErrorMsg.en = 'Error connecting to the server. If error occurs frequently, please disable AJAX by the plugin of the Broser - TYPO3 without PHP. Server error prompt:';
+lang_ajaxErrorMsg.en = 'Error connecting to the server. <br />If error occurs frequently, please disable AJAX by the plugin of the Broser - TYPO3 without PHP.<br />Investigate the error: Firefox console <F12>: console > network.<br />Server error prompt:';
 
 lang_returnToList.de = 'Liste wieder einblenden';
-lang_ajaxErrorMsg.de = 'Fehler bei der Verbindung zum Server. <br />Wenn der Fehler häufiger auftaucht, deaktiviere AJAX im Plugin des Browsers - TYPO3 ohne PHP. Fehler-Meldung des Servers:';
+lang_ajaxErrorMsg.de = 'Fehler bei der Verbindung zum Server. <br />Wenn der Fehler häufiger auftaucht, deaktiviere AJAX im Plugin des Browsers - TYPO3 ohne PHP.<br />Fehler genauer untersuchen? Firefox Konsole <F12>: Konsole > Netz.<br />Fehler-Meldung des Servers:';
 
 lang_returnToList.fr = 'Afficher la liste';
 lang_ajaxErrorMsg.fr = 'Erreur de connexion au serveur:';
