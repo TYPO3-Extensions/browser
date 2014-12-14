d2500ec8511f1f0e8b3bda26c1c831b8c1b06ab4
dwildt <dwildt@735d13b6-9817-0410-8766-e36946ffe9aa>
2014-12-14 20:42:59 +0000

    6.0.7 +++ index browser foundation
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/browser/trunk@87419 735d13b6-9817-0410-8766-e36946ffe9aa

--- pi1/class.tx_browser_pi1_navi_indexBrowser.php
+++ pi1/class.tx_browser_pi1_navi_indexBrowser.php
@@ -2745,17 +2745,17 @@ class tx_browser_pi1_navi_indexBrowser
     $classDefault = $this->pObj->conf[ 'navigation.' ][ 'indexBrowser.' ][ 'classes.' ][ 'tab.' ][ 'default' ];
     $classDefault = str_replace('###KEY###', $key, $classDefault);
     $classDefault = str_replace('###TAB###', $tab[ 'labelAscii' ], $classDefault);
-    if( empty( $classDefault ))
-    {
-      $classDefault = 'ui-state-default ui-corner-top tab-' . $tab[ 'labelAscii' ] . ' tab-' . $key;
-    }
+//    if( empty( $classDefault ))
+//    {
+//      $classDefault = 'ui-state-default ui-corner-top tab-' . $tab[ 'labelAscii' ] . ' tab-' . $key;
+//    }
     $classActive = $this->pObj->conf[ 'navigation.' ][ 'indexBrowser.' ][ 'classes.' ][ 'tab.' ][ 'active' ];
     $classActive = str_replace('###KEY###', $key, $classActive);
     $classActive = str_replace('###TAB###', $tab[ 'labelAscii' ], $classActive);
-    if( empty( $classActive ))
-    {
-      $classActive = 'ui-state-default ui-corner-top tab-' . $tab[ 'labelAscii' ] . ' tab-' . $key . ' ui-tabs-active ui-state-active selected';
-    }
+//    if( empty( $classActive ))
+//    {
+//      $classActive = 'ui-state-default ui-corner-top tab-' . $tab[ 'labelAscii' ] . ' tab-' . $key . ' ui-tabs-active ui-state-active selected';
+//    }
 
     // Selected tab
     $class = $classDefault;
