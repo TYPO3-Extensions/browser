85662530b473c3ba2fccab50038c1e0c48cf3967
dwildt <dwildt@735d13b6-9817-0410-8766-e36946ffe9aa>
2014-12-15 23:38:26 +0000

    6.0.7 +++ category_menu CSS Foundation
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/browser/trunk@87533 735d13b6-9817-0410-8766-e36946ffe9aa

--- Configuration/TypoScript/Foundation/Templating/plugin/tx_browser_pi1/displayList/master_templates/category_menu.ts
+++ Configuration/TypoScript/Foundation/Templating/plugin/tx_browser_pi1/displayList/master_templates/category_menu.ts
@@ -1,4 +1,6 @@
+plugin.tx_browser_pi1.displayList.master_templates.category_menu.first_item.cObject.wrap = <dd class="###ITEM_SELECTED###"><dd><a href="###URL###">|</a></dd>
 plugin.tx_browser_pi1.displayList.master_templates.category_menu.wrap = <dl class="sub-nav"><dt>###TITLE###</dt>|</dl>
 plugin.tx_browser_pi1.displayList.master_templates.category_menu.wrap.object = |
-plugin.tx_browser_pi1.displayList.master_templates.category_menu.wrap.item.cObject.wrap = <dd><a href="###URL###"###CLASS######STYLE######TITLE###>|</a></dd>
+plugin.tx_browser_pi1.displayList.master_templates.category_menu.wrap.item.cObject.wrap = <dd class="###ITEM_SELECTED###"><a href="###URL###">|</a></dd>
 plugin.tx_browser_pi1.displayList.master_templates.category_menu.wrap.item.selected = active
+plugin.tx_browser_pi1.displayList.master_templates.category_menu.wrap.title_stdWrap.wrap = |
