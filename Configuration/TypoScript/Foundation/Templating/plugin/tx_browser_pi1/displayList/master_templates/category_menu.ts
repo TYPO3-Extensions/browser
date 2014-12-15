e2ebedf32aa463d3468fec603a0b8dd76a35c103
dwildt <dwildt@735d13b6-9817-0410-8766-e36946ffe9aa>
2014-12-15 23:13:27 +0000

    6.0.7 +++ category_menu CSS Foundation
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/browser/trunk@87531 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ Configuration/TypoScript/Foundation/Templating/plugin/tx_browser_pi1/displayList/master_templates/category_menu.ts
@@ -0,0 +1,4 @@
+plugin.tx_browser_pi1.displayList.master_templates.category_menu.wrap = <dl class="sub-nav"><dt>###TITLE###</dt>|</dl>
+plugin.tx_browser_pi1.displayList.master_templates.category_menu.wrap.object = |
+plugin.tx_browser_pi1.displayList.master_templates.category_menu.wrap.item.cObject.wrap = <dd><a href="###URL###"###CLASS######STYLE######TITLE###>|</a></dd>
+plugin.tx_browser_pi1.displayList.master_templates.category_menu.wrap.item.selected = active
