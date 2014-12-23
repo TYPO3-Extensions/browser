1c49a0bf911fb8c4736b8e23d5bb8f43583b5b54
dwildt <dwildt@735d13b6-9817-0410-8766-e36946ffe9aa>
2014-12-23 22:42:43 +0000

    6.0.7 +++ #i0117
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/browser/trunk@87750 735d13b6-9817-0410-8766-e36946ffe9aa

--- pi1/class.tx_browser_pi1_filter_4x.php
+++ pi1/class.tx_browser_pi1_filter_4x.php
@@ -4510,6 +4510,7 @@ class tx_browser_pi1_filter_4x
         $cObj_conf = $conf_view[ 'filter.' ][ $table . '.' ][ $field . '.' ][ 'treeview.' ][ 'enabled.' ];
         $treeviewEnabled = $this->pObj->cObj->cObjGetSingle( $cObj_name, $cObj_conf );
 
+var_dump( __METHOD__, __LINE__, $tableField );
         // CONTINUE : field has an dot
         if ( !$treeviewEnabled )
         {
