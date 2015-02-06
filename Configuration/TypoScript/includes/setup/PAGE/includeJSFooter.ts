366e24980208493079c931b534a37b22028d85e9
dwildt <dwildt@735d13b6-9817-0410-8766-e36946ffe9aa>
2015-02-06 16:20:54 +0000

    6.0.8 +++ Map marker pop-up
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/browser/trunk@88337 735d13b6-9817-0410-8766-e36946ffe9aa

--- Configuration/TypoScript/includes/setup/PAGE/includeJSFooter.ts
+++ Configuration/TypoScript/includes/setup/PAGE/includeJSFooter.ts
@@ -1,5 +1,5 @@
 page.includeJSFooter {
-  browserMapToggle = EXT:browser/Resources/Private/Templates/HTML/Map/map_toggle.js
+  browserMapToggle = EXT:browser/Resources/Public/JavaScript/Map/toggle/map_toggle.js
   browserMapToggle {
     if {
       isTrue = {$plugin.tx_browser_pi1.map.html.jss.toggle}
