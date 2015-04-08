d0ed3b44651c83e5552eb31d18d7f4bf4b5c61dc
dwildt <dwildt@735d13b6-9817-0410-8766-e36946ffe9aa>
2015-04-08 10:00:08 +0000

    7.0.6 +++ #i0146
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/browser/trunk@88616 735d13b6-9817-0410-8766-e36946ffe9aa

--- Configuration/TypoScript/samples/tt_news/locallang.ts
+++ Configuration/TypoScript/samples/tt_news/locallang.ts
@@ -1,10 +1,14 @@
 plugin.tx_browser_pi1 {
   _LOCAL_LANG {
     default {
-      tt_news_cat_title  = Category:
+      tt_news_cat_title = Category:
+      list_mode_1000    = List
+      list_mode_1010    = RSS
     }
     de {
       tt_news_cat_title  = Kategorie:
+      list_mode_1000    = Listenansicht
+      list_mode_1010    = RSS-Feed
     }
   }
 }
