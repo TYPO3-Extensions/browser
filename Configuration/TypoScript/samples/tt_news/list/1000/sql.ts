408bed9f57cf4ca1cb04f18c114b9ae5422e7c7f
dwildt <dwildt@735d13b6-9817-0410-8766-e36946ffe9aa>
2015-04-08 09:20:32 +0000

    7.0.6 +++ #i0146
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/browser/trunk@88606 735d13b6-9817-0410-8766-e36946ffe9aa

--- Configuration/TypoScript/samples/tt_news/list/1000/sql.ts
+++ Configuration/TypoScript/samples/tt_news/list/1000/sql.ts
@@ -5,11 +5,14 @@ plugin.tx_browser_pi1 {
         select (
           tt_news.title,
           tt_news_cat.title,
+          tt_news.datetime,
+          tt_news.ext_url,
           tt_news.image,
           tt_news.imagealttext,
           tt_news.imagetitletext,
-          tt_news.datetime,
-          tt_news.short
+          tt_news.page,
+          tt_news.short,
+          tt_news.type
 )
         orderBy (
           tt_news.datetime DESC, tt_news_cat.title, tt_news.title
