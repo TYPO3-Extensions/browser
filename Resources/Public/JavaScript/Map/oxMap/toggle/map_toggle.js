8fd6d5379f3640a1e9bdd07cfe6b0d81e0bc48e4
dwildt <dwildt@735d13b6-9817-0410-8766-e36946ffe9aa>
2015-04-14 07:39:11 +0000

    7.0.10 +++ #i0159
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/browser/trunk@88748 735d13b6-9817-0410-8766-e36946ffe9aa

--- Resources/Public/JavaScript/Map/oxMap/toggle/map_toggle.js
+++ Resources/Public/JavaScript/Map/oxMap/toggle/map_toggle.js
@@ -1,24 +1,25 @@
 /**
  * @description map_toggle.js: toggle the map
  * @author (c) 2015 - Dirk Wildt <http://wildt.at.die-netzmacher.de/>
- * @version 7.0.0
+ * @version 7.0.10
  * @since 6.0.8
  */
+
+var labelSlidedown = "Show the map";
+var labelSlideup = "Hide the map";
+var lnDeSlidedown = "Karte einblenden";
+var lnDeSlideup = "Karte ausblenden";
+
+var htmlLang = $( 'html' ).attr( 'lang' ).substr( 0, 2 );
+
+if( htmlLang === "de" ) {
+  labelSlidedown = lnDeSlidedown;
+  labelSlideup = lnDeSlideup;
+}
 
 $( document ).ready( function( )
 {
 
-  var labelSlidedown = "Show the map";
-  var labelSlideup = "Hide the map";
-  var lnDeSlidedown = "Karte einblenden";
-  var lnDeSlideup = "Karte ausblenden";
-
-  var htmlLang = $( 'html' ).attr( 'lang' ).substr( 0, 2 );
-
-  if( htmlLang === "de" ) {
-    labelSlidedown = lnDeSlidedown;
-    labelSlideup = lnDeSlideup;
-  }
   $( ".maptoggle button" ).html( labelSlideup );
 
   $( ".maptoggle" ).toggle(
