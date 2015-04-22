017c7595dd3e1b30021f478dceba689c2d4d13ba
dwildt <dwildt@735d13b6-9817-0410-8766-e36946ffe9aa>
2015-04-22 20:57:59 +0000

    7.0.15 +++ SECURITY BUGFIX prompt
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/browser/trunk@88887 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -24,7 +24,7 @@ $EM_CONF[ $_EXTKEY ] = array(
   ,
   'category' => 'plugin',
   'shy' => 0,
-  'version' => '7.0.14',
+  'version' => '7.0.15',
   'dependencies' => 'browser_manual_en',
   'conflicts' => 'be_tablefilter',
   'priority' => '',
