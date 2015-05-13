dc215bf2e43910e4fcea7423faeb8bf882f85275
dwildt <dwildt@735d13b6-9817-0410-8766-e36946ffe9aa>
2015-05-13 23:24:58 +0000

    7.1.2 +++ #i0171
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/browser/trunk@89097 735d13b6-9817-0410-8766-e36946ffe9aa

--- Configuration/TypoScript/includes/constants/templates/listview/text.2.ts
+++ Configuration/TypoScript/includes/constants/templates/listview/text.2.ts
@@ -1,12 +1,12 @@
 plugin.tx_browser_pi1 {
 
   # cat=Browser - Templates - List - Text 1*//101;          type=string;    label= Text for listview:Field with the text for the list view. I.e: tx_org_service.teaser_short // tx_org_service.short
-  templates.listview.text.1.field  =
+  templates.listview.text.2.field  =
   # cat=Browser - Templates - List - Text 1*//102;          type=options[div,h1,h2,h3,h4,h5,h6,p];  label= Tag:HTML tag. I.e: p
-  templates.listview.text.1.tag   = p
+  templates.listview.text.2.tag   = p
   # cat=Browser - Templates - List - Text 1*//103;          type=string;    label= Crop text:Crop the text after x chars. Leave it empty, if you don't want any cropping. I.e: 300|...|1
-  templates.listview.text.1.crop    = 300|...|1
+  templates.listview.text.2.crop    = 300|...|1
   # cat=Browser - Templates - List - Text 1*//others/999;   type=user[EXT:browser/lib/class.tx_browser_extmanager.php:tx_browser_extmanager->promptExternalLinks]; label=Helpful links
-  templates.listview.text.1.links = Click me!
+  templates.listview.text.2.links = Click me!
 
 }
--- Configuration/TypoScript/includes/setup/plugin/tx_browser_pi1/displayList/master_templates/tableFields/text.2.ts
+++ Configuration/TypoScript/includes/setup/plugin/tx_browser_pi1/displayList/master_templates/tableFields/text.2.ts
@@ -14,23 +14,23 @@ plugin.tx_browser_pi1 {
         text =
         text {
             // key, default (single view), page, url
-          1 = CASE
-          1 {
+          2 = CASE
+          2 {
             key {
-              field = {$plugin.tx_browser_pi1.templates.listview.url.1.key}
+              field = {$plugin.tx_browser_pi1.templates.listview.url.2.key}
             }
               // single view
             default = COA
             default {
-              wrap  = <{$plugin.tx_browser_pi1.templates.listview.text.1.tag} class="link-to-record">|</{$plugin.tx_browser_pi1.templates.listview.text.1.tag}>
+              wrap  = <{$plugin.tx_browser_pi1.templates.listview.text.2.tag} class="link-to-record">|</{$plugin.tx_browser_pi1.templates.listview.text.2.tag}>
                 // Text
               10 = TEXT
               10 {
-                field     = {$plugin.tx_browser_pi1.templates.listview.text.1.field}
+                field     = {$plugin.tx_browser_pi1.templates.listview.text.2.field}
                 stdWrap {
                   stripHtml         = 1
                   htmlSpecialChars  = 0
-                  crop              = {$plugin.tx_browser_pi1.templates.listview.text.1.crop}
+                  crop              = {$plugin.tx_browser_pi1.templates.listview.text.2.crop}
                 }
               }
                 // Details link
@@ -47,29 +47,29 @@ plugin.tx_browser_pi1 {
                   noTrimWrap        = ||&nbsp;&raquo;|
                 }
                 noTrimWrap = | ||
-                typolink < plugin.tx_browser_pi1.displayList.master_templates.tableFields.typolinks.1.default
+                typolink < plugin.tx_browser_pi1.displayList.master_templates.tableFields.typolinks.2.default
               }
             }
               // without any link (record is available only in list views)
             notype < .default
             notype {
-              wrap  = <{$plugin.tx_browser_pi1.templates.listview.text.1.tag} class="no-link">|</{$plugin.tx_browser_pi1.templates.listview.text.1.tag}>
+              wrap  = <{$plugin.tx_browser_pi1.templates.listview.text.2.tag} class="no-link">|</{$plugin.tx_browser_pi1.templates.listview.text.2.tag}>
               20 >
             }
               // link to an internal page
             page < .default
             page {
-              wrap  = <{$plugin.tx_browser_pi1.templates.listview.text.1.tag} class="link-to-page">|</{$plugin.tx_browser_pi1.templates.listview.text.1.tag}>
+              wrap  = <{$plugin.tx_browser_pi1.templates.listview.text.2.tag} class="link-to-page">|</{$plugin.tx_browser_pi1.templates.listview.text.2.tag}>
               20 {
-                typolink < plugin.tx_browser_pi1.displayList.master_templates.tableFields.typolinks.1.page
+                typolink < plugin.tx_browser_pi1.displayList.master_templates.tableFields.typolinks.2.page
               }
             }
               // link to an external website
             url < .page
             url {
-              wrap  = <{$plugin.tx_browser_pi1.templates.listview.text.1.tag} class="link-to-url">|</{$plugin.tx_browser_pi1.templates.listview.text.1.tag}>
+              wrap  = <{$plugin.tx_browser_pi1.templates.listview.text.2.tag} class="link-to-url">|</{$plugin.tx_browser_pi1.templates.listview.text.2.tag}>
               20 {
-                typolink < plugin.tx_browser_pi1.displayList.master_templates.tableFields.typolinks.1.url
+                typolink < plugin.tx_browser_pi1.displayList.master_templates.tableFields.typolinks.2.url
                 value = URL &raquo;
                 lang {
                   de = URL &raquo;
