6744c68e25545e8e7462d9cb1adb57a6ebae063b
dwildt <dwildt@735d13b6-9817-0410-8766-e36946ffe9aa>
2015-05-13 23:26:50 +0000

    7.1.2 +++ #i0171
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/browser/trunk@89098 735d13b6-9817-0410-8766-e36946ffe9aa

--- Configuration/TypoScript/includes/constants/templates/listview/text.3.ts
+++ Configuration/TypoScript/includes/constants/templates/listview/text.3.ts
@@ -1,12 +1,12 @@
 plugin.tx_browser_pi1 {
 
-  # cat=Browser - Templates - List - Text 1*//101;          type=string;    label= Text for listview:Field with the text for the list view. I.e: tx_org_service.teaser_short // tx_org_service.short
+  # cat=Browser - Templates - List - Text 3*//101;          type=string;    label= Text for listview:Field with the text for the list view. I.e: tx_org_service.teaser_short // tx_org_service.short
   templates.listview.text.3.field  =
-  # cat=Browser - Templates - List - Text 1*//102;          type=options[div,h1,h2,h3,h4,h5,h6,p];  label= Tag:HTML tag. I.e: p
+  # cat=Browser - Templates - List - Text 3*//102;          type=options[div,h1,h2,h3,h4,h5,h6,p];  label= Tag:HTML tag. I.e: p
   templates.listview.text.3.tag   = p
-  # cat=Browser - Templates - List - Text 1*//103;          type=string;    label= Crop text:Crop the text after x chars. Leave it empty, if you don't want any cropping. I.e: 300|...|1
+  # cat=Browser - Templates - List - Text 3*//103;          type=string;    label= Crop text:Crop the text after x chars. Leave it empty, if you don't want any cropping. I.e: 300|...|1
   templates.listview.text.3.crop    = 300|...|1
-  # cat=Browser - Templates - List - Text 1*//others/999;   type=user[EXT:browser/lib/class.tx_browser_extmanager.php:tx_browser_extmanager->promptExternalLinks]; label=Helpful links
+  # cat=Browser - Templates - List - Text 3*//others/999;   type=user[EXT:browser/lib/class.tx_browser_extmanager.php:tx_browser_extmanager->promptExternalLinks]; label=Helpful links
   templates.listview.text.3.links = Click me!
 
 }
