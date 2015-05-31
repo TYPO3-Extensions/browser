5ac7cc2aa67cbdb5ff8b44b9a7359b9465071296
dwildt <dwildt@735d13b6-9817-0410-8766-e36946ffe9aa>
2015-05-31 20:55:21 +0000

    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/browser/trunk@89191 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -24,7 +24,7 @@ $EM_CONF[ $_EXTKEY ] = array(
   ,
   'category' => 'plugin',
   'shy' => 0,
-  'version' => '7.1.4',
+  'version' => '7.2.0',
   'dependencies' => 'browser_manual_en',
   'conflicts' => 'be_tablefilter',
   'priority' => '',
