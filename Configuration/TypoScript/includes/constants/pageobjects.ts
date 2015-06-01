5ed724a8862b954b1340a69f53ff7d7766da7a2f
dwildt <dwildt@735d13b6-9817-0410-8766-e36946ffe9aa>
2015-06-01 08:03:13 +0000

    7.2.0 +++ #i0180
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/browser/trunk@89199 735d13b6-9817-0410-8766-e36946ffe9aa

--- Configuration/TypoScript/includes/constants/pageobjects.ts
+++ Configuration/TypoScript/includes/constants/pageobjects.ts
@@ -4,13 +4,13 @@ plugin.tx_browser_pi1 {
   typeNum.ajaxPageObj = 28562
   # cat=Browser - Page Objects//200;       type=int+;    label= CSV page object: Type number (typenum) of the CSV page object. Default: 29370
   typeNum.csvPageObj = 29370
-  # cat=Browser - Page Objects//201;       type=string;    label= vCard additionalHeaders: additionalHeaders for the vCard page object.
+  # cat=Browser - Page Objects//201;       type=string;    label= CSV additionalHeaders: additionalHeaders for the CSV page object.
   typeNum.csvPageObj.additionalHeaders = Content-Type: text/csv | Content-Disposition: attachment; filename="export.csv"
   # cat=Browser - Page Objects//300;       type=int+;    label= Download page object: Type number (typenum) of the page object for downloads. Default: 31230
   typeNum.downloadPageObj = 31230
-  # cat=Browser - Page Objects//400;       type=int+;    label= Page object for map category markers: Type number (typenum) of the map page object. Default: 32654
+  # cat=Browser - Page Objects//400;       type=int+;    label= MAP page object: Type number (typenum) of the map page object for map category markers. Default: 32654
   typeNum.mapPageObj = 32654
-  # cat=Browser - Page Objects//401;       type=string;    label= vCard additionalHeaders: additionalHeaders for the vCard page object.
+  # cat=Browser - Page Objects//401;       type=string;    label= MAP additionalHeaders: additionalHeaders for the vCard page object.
   typeNum.mapPageObj.additionalHeaders = Content-Type: text/plain
   # cat=Browser - Page Objects//500;       type=int+;    label= vCard page object: Type number (typenum) of the vCard page object. Default: 67208
   typeNum.vCardPageObj = 67208
