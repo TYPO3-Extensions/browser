559c4ad40bbcce1ef7901ed2786cca8e97828214
dwildt <dwildt@735d13b6-9817-0410-8766-e36946ffe9aa>
2015-06-01 22:49:26 +0000

    Update Manual
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/browser/trunk@89206 735d13b6-9817-0410-8766-e36946ffe9aa

--- Configuration/TypoScript/includes/constants/navigation.ts
+++ Configuration/TypoScript/includes/constants/navigation.ts
@@ -1,6 +1,6 @@
 plugin.tx_browser_pi1 {
 
-  # cat=Browser - Navigation//101;            type=string;  label= showUid: Alias for the parameter showUid. WARNING: Set this property for main plugins only, don't set it for plugins in the marginal column. in Alias simplifies the configuration of realUrl. I.e: staffUid.
+  # cat=Browser - Navigation//101;            type=string;  label= showUid: Alias for the parameter showUid. WARNING: Set this property for main plugins only, don't set it for plugins in the marginal column. The alias simplifies the configuration of realUrl. I.e: staffUid.
   navigation.showUid = showUid
   # cat=Browser - Navigation//102;            type=boolean; label= Workaround latin1: Workaround for the index browser in case of trouble with UTF8: Send the SQL query 'SET NAMES latin1' before other any query.
   navigation.indexbrowser.workaroundLatin1 =
